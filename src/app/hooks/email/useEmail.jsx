41fbde17dedc1d1f8072a502a51d01cdf654c03b
bivek.joshi <bivek.joshi@dghub.io>
2023-07-04 13:56:34 +0545

    email

---
+++ src/app/hooks/email/useEmail.jsx
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "react-query";
+import { toast } from "react-toastify";
+import { sendEmail } from "../../api/email/email-api";
+
+export const useSendEmail = ({ employeeId, onSuccess }) => {
+  console.log("use", onSuccess);
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    ["sendEmail"],
+    (formData) => sendEmail({ formData, employeeId }),
+    {
+      onSuccess: (data, variables, context) => {
+        toast.success("Successful send Mail");
+        // Perform any additional logic on success
+        onSuccess && onSuccess(data, variables, context);
+        queryClient.invalidateQueries("sendEmail");
+      },
+      onError: (err, _variables, _context) => {
+        toast.error(`Error: ${err.message}`);
+      },
+    }
+  );
+};
