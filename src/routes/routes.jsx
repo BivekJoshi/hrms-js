55dc549c94a8131f5c4713df66f53d87af0b1ee4
ganesh bhatt <@ganesh.bhatta>
2023-07-05 14:09:07 +0545

    birthday

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -7,40 +7,22 @@ import Attendance from "../app/pages/Attendance/Attendance";
 import Project from "../app/pages/Project/Project";
 import ProjectDetail from "../app/pages/Project/ProjectDetail/ProjectDetail";
 import CustomBreadcrumb from "../theme/overrides/CustomBreadcrumb";
-import { useLocation } from "react-router-dom";
 import DeactivatedProject from "../app/pages/Project/DeactivatedProject/DeactivatedProject";
-
-
-const TodoList = Loadable(lazy(() => import('../app/pages/TodoList/TodoList')));
-const Event = Loadable(lazy(() => import('../app/pages/Event/Event')));
-const Holiday = Loadable(lazy(() => import('../app/pages/Holiday/Holiday')));
-import TodoList from "../app/pages/TodoList/TodoList";
-
 import { useLocation } from "react-router-dom";
-import CustomBreadcrumb from "../theme/overrides/CustomBreadcrumb";
 import EmailForm from "../app/pages/Email/Email";
 
-const Dashboard = Loadable(
-  lazy(() => import("../app/pages/Dashboard/Dashboard"))
-);
+const TodoList = Loadable(lazy(() => import("../app/pages/TodoList/TodoList")));
+const Event = Loadable(lazy(() => import("../app/pages/Event/Event")));
+const Holiday = Loadable(lazy(() => import("../app/pages/Holiday/Holiday")));
+const Dashboard = Loadable(lazy(() => import("../app/pages/Dashboard/Dashboard")));
 const Employee = Loadable(lazy(() => import("../app/pages/Employee/Employee")));
-const Designation = Loadable(
-  lazy(() => import("../app/pages/Designation/Designation"))
-);
-const Department = Loadable(
-  lazy(() => import("../app/pages/Department/Department"))
-);
+const Designation = Loadable(lazy(() => import("../app/pages/Designation/Designation")));
+const Department = Loadable(lazy(() => import("../app/pages/Department/Department")));
 const Company = Loadable(lazy(() => import("../app/pages/Company/Company")));
-const LeaveType = Loadable(
-  lazy(() => import("../app/pages/LeaveType/LeaveType"))
-);
+const LeaveType = Loadable(lazy(() => import("../app/pages/LeaveType/LeaveType")));
 const Leave = Loadable(lazy(() => import("../app/pages/Leave/Leave")));
-const EditEmployee = Loadable(
-  lazy(() => import("../app/pages/Employee/AddEmployee/EditEmployee"))
-);
-const EmployeeViewPage = Loadable(
-  lazy(() => import("../app/pages/Employee/EmployeeViewPage/EmployeeViewPage"))
-);
+const EditEmployee = Loadable(lazy(() => import("../app/pages/Employee/AddEmployee/EditEmployee")));
+const EmployeeViewPage = Loadable(lazy(() => import("../app/pages/Employee/EmployeeViewPage/EmployeeViewPage")));
 
 const routes = [
   {
