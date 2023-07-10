93d8a7aa68e872cfe9ed8e5a99a3bddcc7e08d0f
Sujana Amgain <sujana.amgain@dghub.io>
2023-07-10 14:53:53 +0545

    pagination complete fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 // import { Provider } from "react-redux";
 // import {store} from "./Redux/Store/index.js";
-=======
->>>>>>> caf6db4f0073b4af270ad63662c017f1422fca47
 import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -19,7 +16,6 @@ const queryClient = new QueryClient({
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-<<<<<<< HEAD
   // <Provider store={store}>
     <React.StrictMode>
       <ErrorBoundary>
@@ -31,15 +27,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </ErrorBoundary>
     </React.StrictMode>
   // </Provider>
-=======
-  <React.StrictMode>
-    <ErrorBoundary>
-      <QueryClientProvider client={queryClient}>
-        <ThemeContextProvider>
-          <App />
-        </ThemeContextProvider>
-      </QueryClientProvider>
-    </ErrorBoundary>
-  </React.StrictMode>
->>>>>>> caf6db4f0073b4af270ad63662c017f1422fca47
 );
