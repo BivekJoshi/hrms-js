4e3ad3595b033e7f8c5f33ee20963224163a8363
ganesh bhatt <@ganesh.bhatta>
2023-07-19 10:37:10 +0545

    msg

--- src/app/pages/Department/Department.jsx
+++ src/app/pages/Department/Department.jsx
@@ -1,19 +1,52 @@
 import * as React from "react";
 import { useState } from "react";
 import MaterialTable from "material-table";
-// import MaterialTable from '@material-table/core';
 import { Box, Button, Stack } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
-import {
-  useDeleteDepartment,
-  useGetDepartment,
-} from "../../hooks/department/useDepartment";
-import {
-  AddDepartmentModal,
-  EditDepartmentModal,
-} from "./DepartmentModal/DepartmentModal";
+import { useDeleteDepartment, useGetDepartment } from "../../hooks/department/useDepartment";
+import { AddDepartmentModal, EditDepartmentModal } from "./DepartmentModal/DepartmentModal";
 import DeleteConfirmationModal from "../../components/Modal/DeleteConfirmationModal";
+import { forwardRef } from 'react';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
+
+
+
 
 const Department = () => {
   const { data: departmentData, isLoading } = useGetDepartment();
@@ -46,6 +79,12 @@ const Department = () => {
     setEditedDepartment(rowData);
     setOpenEditModal(true);
   };
+
+
+
+
+
+
   const columns = [
     {
       title: "SN",
@@ -85,8 +124,6 @@ const Department = () => {
           </Button>
         </Stack>
       ),
-      sorting: false,
-      width: "1vh",
     },
   ];
   if (isLoading) return <>Loading</>;
@@ -106,28 +143,27 @@ const Department = () => {
       <br></br>
 
       <MaterialTable
-	  title="Department Data"
+      icons={tableIcons}
+        title="Department Data"
         columns={columns}
         data={departmentData}
-        
         isLoading={isLoading}
         options={{
-			exportButton: true
-        //   padding: "dense",
-        //   margin: 50,
-        //   pageSize: 10,
-        //   emptyRowsWhenPaging: false,
-        //   headerStyle: {
-        //     backgroundColor: "#01579b",
-        //     color: "#FFF",
-        //     fontSize: 20,
-        //     padding: "dense",
-        //     height: 50,
-        //   },
-        //   rowStyle: {
-        //     fontSize: 18,
-        //   },
-		 
+          exportButton: true,
+            padding: "dense",
+            margin: 50,
+            pageSize: 10,
+            emptyRowsWhenPaging: false,
+            headerStyle: {
+              backgroundColor: "#01579b",
+              color: "#FFF",
+              fontSize: 20,
+              padding: "dense",
+              height: 50,
+            },
+            rowStyle: {
+              fontSize: 18,
+            },
         }}
       />
 
