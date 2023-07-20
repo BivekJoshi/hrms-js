53e12ed0c6caee3f33c777f480dd51cb97518383
Sujana Amgain <sujana.amgain@dghub.io>
2023-07-20 09:17:08 +0000

    Pagination

--- src/app/pages/Employee/EmployeeViewPage/EmployProfile/EmployeeProfile.jsx
+++ src/app/pages/Employee/EmployeeViewPage/EmployProfile/EmployeeProfile.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import { useGetEmployeeById } from "../../../../hooks/employee/useEmployee";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { Button, Avatar, Typography, Box } from "@mui/material";
+import { Button } from "@mui/material";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 import "./Style/Style.css";
 import { DetailProfile } from "./Component/DetailProfile";
 import { PersonalProfile } from "./Component/PersonalProfile";
 import ProgressById from "../../ProgressEmployeeData/ProgressById";
-
-const primaryColor = "#1c7ed6";
 
 export const EmployeeProfile = () => {
   const { id } = useParams();
