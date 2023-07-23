26cbd763d45adcd87685253ede187734002e286b
Sujana Amgain <sujana.amgain@dghub.io>
2023-07-23 11:03:50 +0545

    Holiday Calinder Design

--- src/app/pages/Holiday/CurrentHoliday.jsx
+++ src/app/pages/Holiday/CurrentHoliday.jsx
@@ -1,54 +1,67 @@
-import React from 'react';
-import { useGetHolidayCurrent } from '../../hooks/holiday/useHoliday';
-import { Paper, Typography } from '@mui/material';
-import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator, timelineItemClasses } from '@mui/lab';
+import React from "react";
+import { useGetHolidayCurrent } from "../../hooks/holiday/useHoliday";
+import { Box, Paper, Typography } from "@mui/material";
+import { Timeline, TimelineContent, TimelineDot, TimelineItem } from "@mui/lab";
+import { TimelineSeparator, timelineItemClasses } from "@mui/lab";
 
 const CurrentHoliday = () => {
-    const { data: currentData } = useGetHolidayCurrent();
+  const { data: currentData } = useGetHolidayCurrent();
 
-    // Get the current month
-    const currentDate = new Date();
-    const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
+  // Get the current month
+  const currentDate = new Date();
+  const currentMonth = currentDate.toLocaleString("default", { month: "long" });
 
-    return (
-        <Paper sx={{ backgroundColor: "#76818d", height: "90vh", padding: 3, color: "#fff" }}>
-            <h1> {currentMonth}</h1>
-            {currentData && currentData.map((item) => (
-                <Timeline key={item.id}
-                    sx={{
-                        [`& .${timelineItemClasses.root}:before`]: {
-                            flex: 0,
-                            padding: 0,
-                        },
-                    }}
+  return (
+    <Paper
+      sx={{
+        backgroundColor: "#76818d",
+        height: "90vh",
+        padding: 3,
+        color: "#fff",
+      }}
+    >
+      <h1> {currentMonth}</h1>
+      {currentData &&
+        currentData.map((item) => (
+          <Timeline
+            key={item.id}
+            sx={{
+              [`& .${timelineItemClasses.root}:before`]: {
+                flex: 0,
+                padding: 0,
+              },
+              margin:" 0 !important",
+              padding:"0 16px"
+            }}
+          >
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                {/* <TimelineConnector /> */}
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography
+                  sx={{
+                    display: "flex",
+                    fontWeight: "bolder",
+                    fontSize: "larger",
+                  }}
                 >
-                    <TimelineItem>
-                        <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
-                        </TimelineSeparator>
-                        <TimelineContent>
-                            <Typography
-                                style={{
-                                    // color: primaryColor,
-                                    fontWeight: 'bolder',
-                                    fontSize: 'larger',
-                                }}
-                            >
-                                {item.holidayName}
-                            </Typography>
-                            <Typography style={{ fontWeight: 'bolder', fontSize: 'larger' }}>
+                  {item.holidayName}
+                  <Box marginLeft=".5rem"> {item.holidayDate}</Box>
+                </Typography>
+                {/* <Typography style={{ fontWeight: 'bolder', fontSize: 'larger' }}>
                                 {item.holidayDate}
-                            </Typography>
-                            <Typography style={{ fontSize: '13px' }}>
+                            </Typography> */}
+                {/* <Typography style={{ fontSize: '13px' }}>
                                 {item.holidayDescription}
-                            </Typography>
-                        </TimelineContent>
-                    </TimelineItem>
-                </Timeline>
-            ))}
-        </Paper>
-    );
+                            </Typography> */}
+              </TimelineContent>
+            </TimelineItem>
+          </Timeline>
+        ))}
+    </Paper>
+  );
 };
 
 export default CurrentHoliday;
