6ac18daf949385baa981aa107697347c99b2794e
Sujana Amgain <sujana.amgain@dghub.io>
2023-08-02 14:28:36 +0545

    header birthday

--- src/app/pages/Dashboard/Dashboard.jsx
+++ src/app/pages/Dashboard/Dashboard.jsx
@@ -56,9 +56,9 @@ const Dashboard = () => {
       <Box
         sx={{
           display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
+          gridTemplateColumns: "repeat(auto-fit, minmax(400px, 1fr))",
           gap: "2rem",
-          justifySelf: { xs: "center", lg: "auto" },
+          // justifySelf: { xs: "center", lg: "auto" },
         }}
       >
         <BarChatDiagram data={dashboardData} />
