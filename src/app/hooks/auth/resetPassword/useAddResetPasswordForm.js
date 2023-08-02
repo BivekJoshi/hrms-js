2542b7be8fd484288eab78f491cab9d4ea5b3020
Bivek Prasad Joshi <bivek.joshi@dghub.io>
2023-08-02 04:16:16 +0000

    Calender

--- src/app/hooks/auth/resetPassword/useAddResetPasswordForm.js
+++ src/app/hooks/auth/resetPassword/useAddResetPasswordForm.js
@@ -1,30 +1,59 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import { useAddResetPassword, useGetLoggedInUser } from '../usePassword';
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import { useAddResetPassword, useGetLoggedInUser } from "../usePassword";
 
 const useAddForgotPasswordForm = () => {
-    const { data: loggedInUser } = useGetLoggedInUser();
-    
-    const { mutate } = useAddResetPassword({});
+  const [loading, setLoading] = useState(false);
+  const [showValues, setShowValues] = useState({
+    password: "",
+    showPassword: false,
+  });
+  const { data: loggedInUser } = useGetLoggedInUser();
 
-    const formik = useFormik({
-        initialValues: {
-            id: loggedInUser?.id,
-            password: "",
-        },
-        onSubmit: (values) => {
-            handleRequest(values);
-        },
-    });
+  const { mutate } = useAddResetPassword({});
 
-    const handleRequest = (values) => {
-        values = {
-            ...values,
-        };
-        mutate(values, formik, { onSuccess: () => formik.handleReset() });
+  const formik = useFormik({
+    initialValues: {
+      id: loggedInUser?.id,
+      password: "",
+    },
+    // validationSchema: loginSchema,
+    onSubmit: (values) => {
+      setLoading(true);
+      handleRequest(values);
+    },
+  });
+
+  const handleRequest = (values) => {
+    values = {
+      ...values,
     };
+    mutate(
+      values,
+      formik,
+      { onSuccess: () => formik.handleReset() },
+      { onSettled: () => setLoading(false) }
+    );
+  };
 
-    return { formik };
+  const handleClickShowPassword = () => {
+    setShowValues({
+      ...showValues,
+      showPassword: !showValues.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return {
+    formik,
+    loading,
+    showValues,
+    handleClickShowPassword,
+    handleMouseDownPassword,
+  };
 };
 
 export default useAddForgotPasswordForm;
