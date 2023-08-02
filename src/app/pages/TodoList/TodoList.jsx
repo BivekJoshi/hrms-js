a400a71abe3af6a701ce3bacc42fc05463be2659
bivek.joshi <bivek.joshi@dghub.io>
2023-08-02 22:08:57 +0545

    leave

--- src/app/pages/TodoList/TodoList.jsx
+++ src/app/pages/TodoList/TodoList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import MaterialTable from "@material-table/core";
+import MaterialTable from "material-table";
 import { Box, Button, Chip, Stack } from "@mui/material";
 import Skeleton from "@mui/material/Skeleton";
 
@@ -11,6 +11,7 @@ import { AddTodoListModal, EditTodoListModal } from "./TodoModal/TodoModal";
 
 import DeleteIcon from "@mui/icons-material/Delete";
 import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
+import tableIcons from "../../../theme/overrides/TableIcon";
 
 const TodoList = () => {
   const { data: todoListData, isLoading } = useGetTodoList();
@@ -38,30 +39,29 @@ const TodoList = () => {
 
   const columns = [
     {
-      title: 'SN',
-      render: (rowData) => rowData.tableData.index + 1,
+      title: "SN",
+      render: (rowData) => rowData.tableData.id + 1,
       width: "50px",
       sortable: false,
     },
     {
       title: "Message",
       field: "message",
-      width: "100px"
+      width: "100px",
     },
     {
       title: "Due",
       field: "dueDate",
       width: "80px",
       // type: 'numeric',
-      
     },
     {
-      title: 'Priority',
-      field: 'priority',
-      emptyValue: '-',
+      title: "Priority",
+      field: "priority",
+      emptyValue: "-",
       width: "180px",
       cellStyle: {
-        whiteSpace: 'nowrap',
+        whiteSpace: "nowrap",
       },
       render: (rowData) => {
         const priority = rowData.priority;
@@ -76,10 +76,16 @@ const TodoList = () => {
         }
 
         return (
-          <Chip label={priority} style={{ backgroundColor: chipColor, color: "white", width: ' 9rem' }} />
+          <Chip
+            label={priority}
+            style={{
+              backgroundColor: chipColor,
+              color: "white",
+              width: " 9rem",
+            }}
+          />
         );
       },
-
     },
     {
       title: "Actions",
@@ -97,7 +103,7 @@ const TodoList = () => {
         </Stack>
       ),
       sorting: false,
-      width: "80px",
+      width: "3",
     },
   ];
 
@@ -123,6 +129,7 @@ const TodoList = () => {
       </Box>
       <br />
       <MaterialTable
+        icons={tableIcons}
         columns={columns}
         data={todoListData}
         title="Todo List"
@@ -133,16 +140,16 @@ const TodoList = () => {
           pageSize: 10,
           emptyRowsWhenPaging: false,
           headerStyle: {
-            backgroundColor: '#01579b',
-            color: '#FFF',
+            backgroundColor: "#01579b",
+            color: "#FFF",
             fontSize: "1rem",
-            padding: 'dense',
+            padding: "dense",
             height: 50,
-            textAlign:'center',
-            border:'2px solid #fff',
-            minHeight:'10px',
-            textTransform:'capitilize'
-        },
+            textAlign: "center",
+            border: "2px solid #fff",
+            minHeight: "10px",
+            textTransform: "capitilize",
+          },
           rowStyle: {
             fontSize: ".8rem",
           },
