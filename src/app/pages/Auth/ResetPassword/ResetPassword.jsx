3f7e59347bc672d50af38ff4d4ce248bfb89de95
bivek.joshi <bivek.joshi@dghub.io>
2023-08-02 06:54:32 +0545

    reset Password

--- src/app/pages/Auth/ResetPassword/ResetPassword.jsx
+++ src/app/pages/Auth/ResetPassword/ResetPassword.jsx
@@ -1,32 +1,67 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
-import forget from "../../../../assets/forget.avif";
 import useAddResetPasswordForm from "../../../hooks/auth/resetPassword/useAddResetPasswordForm";
-import { Grid, Button, TextField, Stack } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import {
+  Grid,
+  Button,
+  TextField,
+  Stack,
+  Typography,
+  InputAdornment,
+  Tooltip,
+  IconButton,
+} from "@mui/material";
 import usePasswordValidation from "./usePasswordValidation";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+import LockPersonIcon from "@mui/icons-material/LockPerson";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { LoadingButton } from "@mui/lab";
 
 function ValidationItem(props) {
   return (
-    <div className={props.validated ? "validated" : "not-validated"}>
-      {props.validated ? (
-        <h1 style={{ color: "green" }}>
-          {/* <FiCheckCircle /> */}
-          {props.message}
-        </h1>
-      ) : (
-        <h1 style={{ color: "red" }}>
-          {/* <CiCircleRemove /> */}
-          {props.message}
-        </h1>
-      )}
-    </div>
+    <>
+      <div className={props.validated ? "validated" : "not-validated"}>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{ fontWeight: "90rem", color: "#888888" }}
+        >
+          {props.validated ? (
+            <>
+              <CheckCircleOutlineIcon sx={{ color: "green" }} />
+              {props.message}
+            </>
+          ) : (
+            <>
+              <CancelOutlinedIcon sx={{ color: "red" }} />
+              {props.message}
+            </>
+          )}
+        </Typography>
+      </div>
+    </>
   );
 }
 
-const ResetPassword = ({ onClose, isLoading }) => {
-  const navigate = useNavigate();
-  const { formik } = useAddResetPasswordForm();
+const ResetPassword = ({ isLoading }) => {
+  const {
+    formik,
+    showValues,
+    handleClickShowPassword,
+    loading,
+    handleMouseDownPassword,
+  } = useAddResetPasswordForm();
+
+  const {
+    lowerValidated,
+    upperValidated,
+    numberValidated,
+    specialValidated,
+    lengthValidated,
+    handleChangeValidation,
+  } = usePasswordValidation();
+
 
   const handleFormSubmit = async () => {
     const isValid = await formik.validateForm();
@@ -41,29 +76,16 @@ const ResetPassword = ({ onClose, isLoading }) => {
     }
   };
 
-  const handleCancel = () => {
-    navigate("/admin/dashboard");
-  };
-
   const style = {
     display: "flex",
     width: "40%",
     justifyContent: "Column",
     padding: "1rem",
-    border: "9px solid rgb(69 24 247)",
-    boxShadow: "rgba(0, 0, 0, 0.5) 12px 12px 7px",
-    background: "#3edbeb",
+    // border: "9px solid rgb(69 24 247)",
+    // boxShadow: "rgba(0, 0, 0, 0.5) 12px 12px 7px",
+    // background: "#3edbeb",
     gap: "1rem",
   };
-
-  const {
-    lowerValidated,
-    upperValidated,
-    numberValidated,
-    specialValidated,
-    lengthValidated,
-    handleChangeValidation,
-  } = usePasswordValidation();
 
   return (
     !isLoading && (
@@ -76,6 +98,11 @@ const ResetPassword = ({ onClose, isLoading }) => {
         }}
       >
         <Stack sx={{ width: "40%", height: "auto" }}>
+          <Typography variant="h4" component="h2">
+            Change Password <LockPersonIcon sx={{ fontSize: "2rem" }} />
+          </Typography>
+          <br />
+          <p>Password must contain :</p>
           <ValidationItem
             validated={lowerValidated}
             message="At least one lowercase letter"
@@ -102,32 +129,55 @@ const ResetPassword = ({ onClose, isLoading }) => {
             <TextField
               id="password"
               name="password"
-              label="password"
-              type="password"
+              label="New Password"
+              // type="password"
               placeholder="Enter your password..."
               fullWidth
+              required
               value={formik.values.password}
-              onChange={formik.handleChange}
+              onChange={(e) => {
+                formik.handleChange(e);
+                handleChangeValidation(e.target.value);
+              }}
               error={formik.touched.password && Boolean(formik.errors.password)}
               helperText={formik.touched.password && formik.errors.password}
               variant="outlined"
               autoFocus
+              type={showValues.showPassword ? "text" : "password"}
               InputLabelProps={{ shrink: true }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Tooltip title="Show Password">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showValues.showPassword ? (
+                          <VisibilityOff />
+                        ) : (
+                          <Visibility />
+                        )}
+                      </IconButton>
+                    </Tooltip>
+                  </InputAdornment>
+                ),
+              }}
             />
           </Grid>
           <Grid item>
             <TextField
               id="confirmPassword"
               name="confirmPassword"
-              label="confirmPassword"
-              type="password"
+              label="Confirm New Password"
+              // type="password"
               placeholder="Confirm Your Password..."
               fullWidth
+              required
               value={formik.values.confirmPassword}
-              onChange={(e) => {
-                formik.handleChange(e);
-                handleChangeValidation(e.target.value);
-              }}
+              onChange={formik.handleChange}
               error={
                 formik.touched.confirmPassword &&
                 Boolean(formik.errors.confirmPassword)
@@ -137,30 +187,46 @@ const ResetPassword = ({ onClose, isLoading }) => {
               }
               variant="outlined"
               autoFocus
+              type={showValues.showPassword ? "text" : "password"}
               InputLabelProps={{ shrink: true }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Tooltip title="Show Password">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showValues.showPassword ? (
+                          <VisibilityOff />
+                        ) : (
+                          <Visibility />
+                        )}
+                      </IconButton>
+                    </Tooltip>
+                  </InputAdornment>
+                ),
+              }}
             />
           </Grid>
 
           <Grid
-            container
-            direction="row"
-            justifyContent="space-around"
-            alignItems="flex-end"
+          // container
+          // direction="row"
+          // justifyContent="space-around"
+          // alignItems="flex-end"
           >
-            <Button
-              variant="contained"
-              onClick={handleCancel}
-              sx={{ mt: 3, ml: 1 }}
-            >
-              cancel
-            </Button>
-            <Button
-              variant="contained"
+            <LoadingButton
+              fullWidth
               onClick={handleFormSubmit}
-              sx={{ mt: 3, ml: 1 }}
+              variant="contained"
+              loading={loading}
+              className="login-btn"
             >
               Change Password
-            </Button>
+            </LoadingButton>
           </Grid>
         </Stack>
       </Grid>
