86271baf8f7f1a7e342538e765508dc0f09da7b5
Sujana Amgain <sujana.amgain@dghub.io>
2023-08-03 05:12:40 +0000

    Stylling

--- src/app/pages/Notification/Notification.jsx
+++ src/app/pages/Notification/Notification.jsx
@@ -17,10 +17,14 @@ const Notification = () => {
   const { data: events } = useGetEvent();
 
   const todayDate = new Date().toISOString().split("T")[0];
-
+  const todayHoliday = holidays?.filter(
+    (event) => event?.holidayDate === todayDate
+  );
   const todayEvent = events?.filter((event) => event?.eventDate === todayDate);
 
-  const notificationNumber = todayEvent?.length;
+  const notificationNumber =
+    (todayEvent?.length ?? 0) +
+    (todayHoliday?.length ?? 0);
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
@@ -45,7 +49,8 @@ const Notification = () => {
         onClick={handleClick}
         style={btnStyle}
       >
-        <NotificationsIcon />{notificationNumber ? notificationNumber : ""}
+        <NotificationsIcon />
+        {notificationNumber}
       </Button>
       <Menu
         id="basic-menu"
@@ -60,21 +65,19 @@ const Notification = () => {
           <List>
             <MenuItem disablePadding>
               <Typography variant="h6" color="primary" fontWeight={400}>
-                Today's Event:
+              {notificationNumber !== 0 ? "Today's Holiday:" : "No Holiday Today!"}
               </Typography>
             </MenuItem>
-            <Divider />
-            {todayEvent
-              ? todayEvent.map((item) => (
-                  <MenuItem key={item.id}>
-                    <ListItemText primary={item?.eventName} />
-                  </MenuItem>
-                ))
-              : "No event for today!"}
+            {notificationNumber !== 0 && <Divider />}
+            {todayHoliday &&
+              todayHoliday.map((item) => (
+                <MenuItem key={item.id}>
+                  <ListItemText primary={item?.holidayName} />
+                </MenuItem>
+              ))}
           </List>
         </Stack>
-
-        <Divider />
+        {notificationNumber !== 0 && <Divider />}
       </Menu>
     </Box>
   );
