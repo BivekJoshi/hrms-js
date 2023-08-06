8306c9ed64b6444e7a96397c2425f1b2fc9deb37
sanjay.thakur <sanjay.thakur@dghub.io>
2023-08-06 17:50:58 +0300

    fix merge conflict

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-<<<<<<< HEAD
 export default defineConfig({
   plugins: [react()],
   base: "",
   server: {
     cors: true,
     // port: 8080,
-=======
-
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  server: {
-    cors: true,
->>>>>>> 5b8b93d4b0713a1ba54a953dac502bf078bb5827
   },
 });
 
