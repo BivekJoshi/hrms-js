1cfe1eed28e15b868ebe0123aca0d0bf77e3bce6
bivek.joshi <bivek.joshi@dghub.io>
2023-08-15 10:29:45 +0545

    leave solved

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,23 +1,21 @@
 import React, { lazy } from "react";
 import ScrollToTop from "../app/utils/ScrolltoTop";
-import { HashRouter, Route, Routes, BrowserRouter } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Applayout from "../layout/Applayout";
 import AdminLayout from "../layout/AdminLayout";
 import ProtectedRoute from "./ProtectedRoute";
 import { routes } from "./routes";
 import ForgotPassword from "../app/pages/Auth/FogotPassword/ForgotPassword";
 import Login from "../app/pages/Auth/Login/Login";
-import RenamePassword from "../app/pages/Auth/ResetPassword/RenamePassword";
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop>
         <Routes>
           <Route path="/" exact element={<Applayout />}>
             <Route path="/" exact element={<Login />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-             <Route path="/hrms/user-activation" element={<RenamePassword />} />
+            <Route path="/forgot-password" exact element={<ForgotPassword />} />
             <Route path="/admin" element={<AdminLayout />}>
               <Route element={<ProtectedRoute redirectTo="/" />}>
                 {routes.map((route) => (
@@ -33,7 +31,7 @@ const AppRoutes = () => {
           </Route>
         </Routes>
       </ScrollToTop>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
