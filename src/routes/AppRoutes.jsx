faead45bbc13fa5eea115a0a81bce68521ace71c
ganesh bhatt <@ganesh.bhatta>
2023-08-17 22:32:21 +0545

    auth

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,11 @@
-import React, { lazy } from "react";
+import React, { lazy, useState, useEffect } from "react";
 import ScrollToTop from "../app/utils/ScrolltoTop";
-import { BrowserRouter as Router, HashRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Applayout from "../layout/Applayout";
 import AdminLayout from "../layout/AdminLayout";
 import ProtectedRoute from "./ProtectedRoute";
@@ -10,41 +15,60 @@ import Login from "../app/pages/Auth/Login/Login";
 import useAuth from "../auth/hooks/component/login/useAuth";
 
 const AppRoutes = () => {
-  const { isSuperAdmin, isAdmin, isHr, isEmployee } = useAuth();
+   const { isSuperAdmin, isAdmin, isEmployee } = useAuth();
+  console.log({
+    superadmin: isSuperAdmin,
+    isadmin: isAdmin,
+    isEmployee: isEmployee,
+  });
 
   return (
-    <Router>
-      <ScrollToTop>
-        <Routes>
-          <Route path="/" exact element={<Applayout />}>
-            <Route path="/" exact element={<Login />} />
-            <Route path="/forgot-password" exact element={<ForgotPassword />} />
-            <Route path="/admin" element={<AdminLayout />}>
-              <Route element={<ProtectedRoute redirectTo="/" />}>
-                {routes.map((route) => {
-                  if (route.path === "users" && isSuperAdmin || isAdmin || isHr ) {
-                    
-                    return (
-                      <Route
-                        key={route.id}
-                        path={route.path}
-                        exact
-                        element={route.component}
-                      />
-                    );
-                  } else if (
-                    route.path === "users" && isEmployee
-                  ) {
-                    return <p>You Don Not Have Access!</p>
-                  }
-                  return null;
-                })}
+    <>
+      <HashRouter>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" exact element={<Applayout />}>
+              <Route path="/" exact element={<Login />} />
+              <Route
+                path="/forgot-password"
+                exact
+                element={<ForgotPassword />}
+              />
+
+              <Route path="/admin" element={<AdminLayout />}>
+                <Route element={<ProtectedRoute redirectTo="/" />}>
+                  
+                  {routes.map((route) => {
+                    if ( (route.path !== "users" && route.path !== "employee") || isSuperAdmin || isAdmin ) {
+                      return (
+                        <Route
+                          key={route.id}
+                          path={route.path}
+                          exact
+                          element={route.component}
+                        />
+                      );
+                    } else if (isEmployee) {
+                      return (
+                        <Route
+                          key={route.id}
+                          path={route.path}
+                          element={
+                            <p>You don't have access to this component.</p>
+                          }
+                        />
+                      );
+                    } else {
+                      return null;
+                    }
+                  })}
+                </Route>
               </Route>
             </Route>
-          </Route>
-        </Routes>
-      </ScrollToTop>
-    </Router>
+          </Routes>
+        </ScrollToTop>
+      </HashRouter>
+    </>
   );
 };
 
