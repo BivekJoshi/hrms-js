465ff891a5644d34c7012fbef8f4725034fd172c
ganesh bhatt <@ganesh.bhatta>
2023-08-23 20:13:09 +0545

    msg ntg

--- src/app/pages/Auth/UserControl/Roles/AddRoleModal.jsx
+++ src/app/pages/Auth/UserControl/Roles/AddRoleModal.jsx
@@ -3,7 +3,6 @@ import FormModal from "../../../../components/Modal/FormModal";
 import { AddRoleFields } from "./AddRoleFields";
 import { useRoleForm } from "../../../../hooks/auth/roles/RoleForm/useRoleForm";
 import { useGetRoleByID } from "../../../../hooks/auth/roles/useRole";
-import RolePermissionField from "./RolePermissionField";
 
 export const AddRoleModal = ({ open, handleCloseModal }) => {
   return (
@@ -22,17 +21,6 @@ export const EditRoleModal = ({ open, handleCloseModal, id }) => {
       open={open}
       onClose={handleCloseModal}
       formComponent={<AddRoleFields onClose={handleCloseModal} data={data} />}
-    />
-  );
-};
-
-export const EditPermissionRoleModal = ({ open, handleCloseModal, id }) => {
-  const { data } = useGetRoleByID(id);
-  return (
-    <FormModal
-      open={open}
-      onClose={handleCloseModal}
-      formComponent={<RolePermissionField onClose={handleCloseModal} data={data} />}
     />
   );
 };
