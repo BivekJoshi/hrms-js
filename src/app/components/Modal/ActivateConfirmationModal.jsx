cb3b345007a07ac54406e0982402712ce479e124
Sujana Amgain <sujana.amgain@dghub.io>
2023-08-24 05:30:36 +0000

    Stylling

--- src/app/components/Modal/ActivateConfirmationModal.jsx
+++ src/app/components/Modal/ActivateConfirmationModal.jsx
@@ -1,32 +1,40 @@
-import React from 'react';
-import { Dialog, DialogActions, DialogContent, DialogTitle, Button, CircularProgress } from '@mui/material';
+import React, { useContext } from "react";
+import { Box, Dialog, DialogActions, DialogContent } from "@mui/material";
+import { DialogTitle, Button, CircularProgress } from "@mui/material";
+import ThemeModeContext from "../../../theme/ThemeModeContext";
 
-const ActivateConfirmationModal = ({ open, handleCloseModal, handleConfirmActive, isLoading ,message}) => {
-    return (
-        <Dialog open={open} onClose={handleCloseModal}>
-            <DialogTitle>Confirm Activation</DialogTitle>
-            <DialogContent>
-                Are you sure you want to Activate this {message}?
-            </DialogContent>
-            <DialogActions>
-                <Button
-                    variant="contained"
-                    onClick={handleCloseModal}
-                    color="error"
-                >
-                    Cancel
-                </Button>
-                <Button
-                    variant="contained"
-                    onClick={handleConfirmActive}
-                    disabled={isLoading}
-                    startIcon={isLoading ? <CircularProgress size={20} /> : null}
-                >
-                    {isLoading ? 'activating...' : 'Activate'}
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+const ActivateConfirmationModal = ({
+  open,
+  handleCloseModal,
+  handleConfirmActive,
+  isLoading,
+  message,
+}) => {
+  const { mode } = useContext(ThemeModeContext);
+
+  return (
+    <Dialog open={open} onClose={handleCloseModal}>
+      <Box sx={{ bgcolor: mode === "light" ? "" : "#413e3e" }}>
+        <DialogTitle>Confirm Activation</DialogTitle>
+        <DialogContent>
+          Are you sure you want to Activate this {message}?
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" onClick={handleCloseModal} color="error">
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            onClick={handleConfirmActive}
+            disabled={isLoading}
+            startIcon={isLoading ? <CircularProgress size={20} /> : null}
+          >
+            {isLoading ? "activating..." : "Activate"}
+          </Button>
+        </DialogActions>
+      </Box>
+    </Dialog>
+  );
 };
 
 export default ActivateConfirmationModal;
