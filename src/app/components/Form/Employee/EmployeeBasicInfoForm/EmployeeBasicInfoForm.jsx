ffc2d9763ff525ee2e61343bc71f4b14c540d187
Sujana Amgain <sujana.amgain@dghub.io>
2023-08-24 11:03:39 +0545

    bug fixing

--- src/app/components/Form/Employee/EmployeeBasicInfoForm/EmployeeBasicInfoForm.jsx
+++ src/app/components/Form/Employee/EmployeeBasicInfoForm/EmployeeBasicInfoForm.jsx
@@ -1,72 +1,82 @@
-import { Grid, MenuItem, TextField } from '@mui/material';
-import React from 'react';
-import { useGetCompany } from '../../../../hooks/company/useCompany';
-import { useGetDesignation } from '../../../../hooks/designation/useDesignation';
-import { useGetDepartment } from '../../../../hooks/department/useDepartment';
+import { Grid, MenuItem, TextField } from "@mui/material";
+import React, { useContext } from "react";
+import { useGetCompany } from "../../../../hooks/company/useCompany";
+import { useGetDesignation } from "../../../../hooks/designation/useDesignation";
+import { useGetDepartment } from "../../../../hooks/department/useDepartment";
+import ThemeModeContext from "../../../../../theme/ThemeModeContext";
 
 const genderOptions = [
   {
-    value: 'MALE',
-    label: 'Male',
+    value: "MALE",
+    label: "Male",
     id: 1,
   },
   {
-    value: 'FEMALE',
-    label: 'Female',
+    value: "FEMALE",
+    label: "Female",
     id: 2,
   },
   {
-    value: 'OTHERS',
-    label: 'Others',
+    value: "OTHERS",
+    label: "Others",
     id: 3,
   },
 ];
 const maritalStatus = [
   {
-    value: 'MARRIED',
-    label: 'Married',
+    value: "MARRIED",
+    label: "Married",
   },
   {
-    value: 'UNMARRIED',
-    label: 'Unmarried',
+    value: "UNMARRIED",
+    label: "Unmarried",
   },
 ];
 
 const currentDate = new Date();
-const minAge = new Date(currentDate.getFullYear() - 18, currentDate.getMonth(), currentDate.getDate()).toISOString().split('T')[0];
-const today = new Date().toISOString().split('T')[0];
+const minAge = new Date(
+  currentDate.getFullYear() - 18,
+  currentDate.getMonth(),
+  currentDate.getDate()
+)
+  .toISOString()
+  .split("T")[0];
+const today = new Date().toISOString().split("T")[0];
 
 const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
   const { data: companyData, isLoading: loadingCompany } = useGetCompany();
-  const { data: designationData, isLoading: loadingDesignation } = useGetDesignation();
-  const { data: departmentData, isLoading: loadingDepartment } = useGetDepartment();
+  const { data: designationData, isLoading: loadingDesignation } =
+    useGetDesignation();
+  const { data: departmentData, isLoading: loadingDepartment } =
+    useGetDepartment();
+  const { mode } = useContext(ThemeModeContext);
 
   return (
     !isLoading && (
       <Grid container spacing={3}>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='firstName'
-            name='firstName'
-            label='First Name'
-            placeholder='Enter your first name'
+            id="firstName"
+            name="firstName"
+            label="First Name"
+            placeholder="Enter your first name"
             fullWidth
             required
             value={formik.values.firstName}
             onChange={formik.handleChange}
             error={formik.touched.firstName && Boolean(formik.errors.firstName)}
             helperText={formik.touched.firstName && formik.errors.firstName}
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           />
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='middleName'
-            name='middleName'
-            label='Middle Name'
-            placeholder='Enter your middle name'
+            id="middleName"
+            name="middleName"
+            label="Middle Name"
+            placeholder="Enter your middle name"
             fullWidth
             value={formik.values.middleName}
             onChange={formik.handleChange}
@@ -74,47 +84,51 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
               formik.touched.middleName && Boolean(formik.errors.middleName)
             }
             helperText={formik.touched.middleName && formik.errors.middleName}
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           />
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='lastName'
-            name='lastName'
-            label='Last Name'
-            placeholder='Enter your last name'
+            id="lastName"
+            name="lastName"
+            label="Last Name"
+            placeholder="Enter your last name"
             fullWidth
             required
             value={formik.values.lastName}
             onChange={formik.handleChange}
             error={formik.touched.lastName && Boolean(formik.errors.lastName)}
             helperText={formik.touched.lastName && formik.errors.lastName}
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           />
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='gender'
-            name='gender'
+            id="gender"
+            name="gender"
             select
-            label='Gender'
-            placeholder='Select your gender'
+            label="Gender"
+            placeholder="Select your gender"
             fullWidth
             required
             value={formik.values.gender}
             onChange={formik.handleChange}
             error={formik.touched.gender && Boolean(formik.errors.gender)}
             helperText={formik.touched.gender && formik.errors.gender}
-            variant='outlined'
+            variant="outlined"
             onBlur={formik.handleBlur}
             InputLabelProps={{ shrink: true }}
           >
             {genderOptions?.map((option) => (
-              <MenuItem key={option?.id} value={option?.value}>
+              <MenuItem
+                key={option?.id}
+                value={option?.value}
+                sx={{ bgcolor: mode === "light" ? "" : "#413e3e" }}
+              >
                 {option?.label}
               </MenuItem>
             ))}
@@ -122,9 +136,9 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            name='dateOfBirth'
-            label='Date of Birth'
-            type='date'
+            name="dateOfBirth"
+            label="Date of Birth"
+            type="date"
             required
             InputLabelProps={{ shrink: true }}
             fullWidth
@@ -141,9 +155,9 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            name='dateOfJoin'
-            label='Date of Join'
-            type='date'
+            name="dateOfJoin"
+            label="Date of Join"
+            type="date"
             required
             InputLabelProps={{ shrink: true }}
             fullWidth
@@ -160,10 +174,10 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='mobileNumber'
-            name='mobileNumber'
-            label='Mobile Number'
-            placeholder='Enter your mobile number'
+            id="mobileNumber"
+            name="mobileNumber"
+            label="Mobile Number"
+            placeholder="Enter your mobile number"
             fullWidth
             required
             value={formik.values.mobileNumber}
@@ -174,17 +188,17 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
             helperText={
               formik.touched.mobileNumber && formik.errors.mobileNumber
             }
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           />
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='CitizenshipNumber'
-            name='citizenshipNumber'
-            label='Citizenship Number'
-            placeholder='Enter citizenship number'
+            id="CitizenshipNumber"
+            name="citizenshipNumber"
+            label="Citizenship Number"
+            placeholder="Enter citizenship number"
             fullWidth
             required
             value={formik.values.citizenshipNumber}
@@ -197,33 +211,33 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
               formik.touched.citizenshipNumber &&
               formik.errors.citizenshipNumber
             }
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           />
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='panNumber'
-            name='panNumber'
-            label='PAN Number'
-            placeholder='Enter PAN number'
+            id="panNumber"
+            name="panNumber"
+            label="PAN Number"
+            placeholder="Enter PAN number"
             fullWidth
             value={formik.values.panNumber}
             onChange={formik.handleChange}
             error={formik.touched.panNumber && Boolean(formik.errors.panNumber)}
             helperText={formik.touched.panNumber && formik.errors.panNumber}
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           />
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='officeEmail'
-            name='officeEmail'
-            label='Office Email'
-            placeholder='Enter office email'
+            id="officeEmail"
+            name="officeEmail"
+            label="Office Email"
+            placeholder="Enter office email"
             fullWidth
             required
             value={formik.values.officeEmail}
@@ -232,18 +246,18 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
               formik.touched.officeEmail && Boolean(formik.errors.officeEmail)
             }
             helperText={formik.touched.officeEmail && formik.errors.officeEmail}
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           />
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='maritalStatus'
-            name='maritalStatus'
+            id="maritalStatus"
+            name="maritalStatus"
             select
-            label='Marital Status'
-            placeholder='Select marital status'
+            label="Marital Status"
+            placeholder="Select marital status"
             fullWidth
             required
             value={formik.values.maritalStatus}
@@ -255,12 +269,16 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
             helperText={
               formik.touched.maritalStatus && formik.errors.maritalStatus
             }
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           >
             {maritalStatus.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
+              <MenuItem
+                key={option.value}
+                value={option.value}
+                sx={{ bgcolor: mode === "light" ? "" : "#413e3e" }}
+              >
                 {option.label}
               </MenuItem>
             ))}
@@ -268,24 +286,28 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='companyId'
-            name='companyId'
+            id="companyId"
+            name="companyId"
             select
-            label='Company Name'
-            placeholder='Select your company'
+            label="Company Name"
+            placeholder="Select your company"
             fullWidth
             required
             value={!loadingCompany && formik.values.companyId}
             onChange={formik.handleChange}
             error={formik.touched.companyId && Boolean(formik.errors.companyId)}
             helperText={formik.touched.companyId && formik.errors.companyId}
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           >
             {!loadingCompany &&
               companyData.map((option) => (
-                <MenuItem key={option?.id} value={option?.id}>
+                <MenuItem
+                  key={option?.id}
+                  value={option?.id}
+                  sx={{ bgcolor: mode === "light" ? "" : "#413e3e" }}
+                >
                   {option?.companyName}
                 </MenuItem>
               ))}
@@ -293,11 +315,11 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='positionId'
-            name='positionId'
+            id="positionId"
+            name="positionId"
             select
-            label='Designation'
-            placeholder='Select your designation'
+            label="Designation"
+            placeholder="Select your designation"
             fullWidth
             required
             value={!loadingDesignation && formik.values.positionId}
@@ -306,13 +328,17 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
               formik.touched.positionId && Boolean(formik.errors.positionId)
             }
             helperText={formik.touched.positionId && formik.errors.positionId}
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           >
             {!loadingDesignation &&
               designationData.map((option) => (
-                <MenuItem key={option.id} value={option.id}>
+                <MenuItem
+                  key={option.id}
+                  value={option.id}
+                  sx={{ bgcolor: mode === "light" ? "" : "#413e3e" }}
+                >
                   {option.positionName} ({option.positionLevel})
                 </MenuItem>
               ))}
@@ -320,11 +346,11 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <TextField
-            id='departmentId'
-            name='departmentId'
+            id="departmentId"
+            name="departmentId"
             select
-            label='Department Name'
-            placeholder='Select your department'
+            label="Department Name"
+            placeholder="Select your department"
             fullWidth
             required
             value={!loadingDepartment && formik.values.departmentId}
@@ -335,13 +361,17 @@ const EmployeeBasicInfoForm = ({ formik, isLoading }) => {
             helperText={
               formik.touched.departmentId && formik.errors.departmentId
             }
-            variant='outlined'
+            variant="outlined"
             autoFocus
             InputLabelProps={{ shrink: true }}
           >
             {!loadingDepartment &&
               departmentData.map((option) => (
-                <MenuItem key={option.id} value={option.id}>
+                <MenuItem
+                  key={option.id}
+                  value={option.id}
+                  sx={{ bgcolor: mode === "light" ? "" : "#413e3e" }}
+                >
                   {option.departmentName}
                 </MenuItem>
               ))}
