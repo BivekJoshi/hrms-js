bee61c13528f16a75a180a302efd864e1c216ac0
Sujana Amgain <sujana.amgain@dghub.io>
2023-08-30 15:22:33 +0545

    employ deshboard api integrate

--- src/app/pages/EmployeePage/EmployeeDashboard/EmployeeDashbord.jsx
+++ src/app/pages/EmployeePage/EmployeeDashboard/EmployeeDashbord.jsx
@@ -5,23 +5,34 @@ import "../../Style/Style.css";
 import { LeftEmployDashbord } from "./LeftEmployDashbord";
 import { RightEmployDashbord } from "./RightEmployDashbord";
 import ThemeModeContext from "../../../../theme/ThemeModeContext";
+import { useGetLoggedInUser } from "../../../hooks/auth/usePassword";
 
 const EmployeeDashbord = (props) => {
+  const { data: employData } = useGetLoggedInUser();
   const { mode } = useContext(ThemeModeContext);
+
+  const today = new Date();
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  };
+  const formattedDate = today.toLocaleDateString(undefined, options);
+
   return (
     <Box display="grid" gridTemplateRows="1fr" gap="1rem">
       <Box
         display="flex"
         flexDirection="row"
-       
         className={
           mode === "light" ? "employeeDeshbordBG" : "employeeDeshbordBGDark"
         }
       >
         <img src={Male} alt="profile" />
         <Box alignSelf="center">
-          <h3>Welcome , Vivek</h3>
-          <h3> Thursday, May 1, 2023</h3>
+          <h3>Welcome , {employData.name}</h3>
+          <h3>{formattedDate}</h3>
         </Box>
       </Box>
       <Box display="grid" gridTemplateColumns="3fr 2fr" gap="3rem">
