375aa6a0727cdadb69cd037ddbf654c92dd79db2
Sujana Amgain <sujana.amgain@dghub.io>
2023-09-11 12:03:10 +0545

    employ project team data not available

--- src/app/pages/Project/ProjectEmployeeViewPage/component/ProjectTeamPage.jsx
+++ src/app/pages/Project/ProjectEmployeeViewPage/component/ProjectTeamPage.jsx
@@ -45,9 +45,55 @@ console.log(logInUserData);
     },
   ];
   return (
-    <div>
-      Hello
-    </div>
+    <Box>
+      {logInUserData ? (
+        logInUserData.map((project, index) => (
+          <Box key={index}>
+            <h3 style={{ padding: " 0 0 1rem 0 " }}>{project.name}</h3>
+            <Box>
+              {project?.projectEmployees?.map((employ) => (
+                <Box key={employ.empId}>
+                  {employ.assignedOn}
+                  <Grid
+                    container
+                    item
+                    gap={1}
+                    className="project-card-control"
+                    sx={{
+                      display: "grid",
+                      gridTemplateColumns:
+                        "repeat(auto-fit, minmax(250px, 1fr))",
+                      gap: "1rem",
+                    }}
+                  >
+                    {/* {employeeData?.map((employee, index) => (
+                    <EmployeeCard
+                      key={index}
+                      IsActive={employee?.isActive || ""}
+                      EmployeeId={employee?.id || ""}
+                      EFirstName={employee?.firstName || ""}
+                      EMiddleName={employee?.middleName || ""}
+                      ELastName={employee?.lastName || ""}
+                      OfficeEmail={employee?.officeEmail || ""}
+                      MobileNumber={employee?.mobileNumber || ""}
+                      // PositionName={employee?.position?.positionName || ""}
+                      // PositionLevel={employee?.position?.positionLevel || ""}
+                      EGender={employee?.gender || ""}
+                      // EmployeeData={employeeData}
+                      // ProgressBarRes={employee?.progressBarRes || ""}
+                      // employeePhoto={employee?.employeePhotoPath}
+                    />
+                  ))} */}
+                  </Grid>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+        ))
+      ) : (
+        <p>Loading or no data available</p>
+      )}
+    </Box>
   );
 }
 
