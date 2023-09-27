56e12bda882e774e330bdd2deac4a1f356568ad5
sanjay.thakur <sanjay.thakur@dghub.io>
2023-09-27 10:21:21 +0545

    routes fixed

---
+++ src/routes/LoggedInRoutes.jsx
@@ -0,0 +1,26 @@
+import  { useEffect } from 'react'
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
+import { getUser, removeUser } from '../app/utils/cookieHelper';
+import { toast } from 'react-toastify';
+import jwtDecode from 'jwt-decode';
+
+const LoggedInRoutes = ({ redirectTo, isRole }) => {    
+    
+    const navigate = useNavigate();
+    const user = getUser();
+    const decode = jwtDecode(user);
+    const userRole = decode?.userRole;
+    console.log("🚀 ~ file: LoggedInRoutes.jsx:13 ~ LoggedInRoutes ~ userRole:", userRole)
+    useEffect(() => {
+        if (!user) {
+          removeUser();
+          navigate("/");
+          toast.warn("Login To Access The Link");
+        } 
+        // eslint-disable-next-line
+      }, []);
+      if (isRole !== userRole) return <Navigate exact to={redirectTo} />;
+      return <Outlet />;
+}
+
+export default LoggedInRoutes;
