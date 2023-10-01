24bb4dabfdf438d960fe61d64f0f48d2a8589f12
Sujana Amgain <sujana.amgain@dghub.io>
2023-10-01 11:40:16 +0000

    Logistic

--- src/app/hooks/resource/employeeResource/EmployeeResourceForm/useEmployeeResourceForm.js
+++ src/app/hooks/resource/employeeResource/EmployeeResourceForm/useEmployeeResourceForm.js
@@ -1,18 +1,21 @@
-import { useAddEmployeeResource, useEditEmployeeResource } from "../useEmployeeResource";
+import {
+  useAddEmployeeResource,
+  useEditEmployeeResource,
+} from "../useEmployeeResource";
 import { useFormik } from "formik";
 import { EmployeeResourceSchema } from "./EmployeeResourceSchema";
 
 const useEmployeeResourceForm = (data) => {
-  const { mutate :addEmployeeResource} = useAddEmployeeResource({});
-  const { mutate :editEmployeeResource} = useEditEmployeeResource({});
-  
+  const { mutate: addEmployeeResource } = useAddEmployeeResource({});
+  const { mutate: editEmployeeResource } = useEditEmployeeResource({});
+
   const formik = useFormik({
     initialValues: {
-      officeResourceId: data?.officeResourceId||"",
-      employeeId: data?.employeeId||"",
-      receiveDate: data?.receiveDate||"",
-      returnDate: data?.returnDate||"",
-      id: data?.id ||"",
+      officeResourceId: data?.officeResourceId || "",
+      employeeId: data?.employeeId || "",
+      receiveDate: data?.receiveDate || "",
+      returnDate: data?.returnDate || "",
+      id: data?.id || "",
     },
     validationSchema: EmployeeResourceSchema,
     enableReinitialize: true,
