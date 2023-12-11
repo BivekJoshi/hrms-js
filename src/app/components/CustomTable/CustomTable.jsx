c4d542dff378013646a34075fff5dc91db7a24df
sanjay.thakur <sanjay.thakur@dghub.io>
2023-12-11 10:07:41 +0545

    fixes

--- src/app/components/CustomTable/CustomTable.jsx
+++ src/app/components/CustomTable/CustomTable.jsx
@@ -1,7 +1,7 @@
-import MaterialTable from "material-table";
-import React, { useContext } from "react";
-import ThemeModeContext from "../../../theme/ThemeModeContext";
-import tableIcons from "../../../theme/overrides/TableIcon";
+import MaterialTable from 'material-table';
+import React, { useContext } from 'react';
+import ThemeModeContext from '../../../theme/ThemeModeContext';
+import tableIcons from '../../../theme/overrides/TableIcon';
 
 const CustomTable = (props) => {
   const { palette } = useContext(ThemeModeContext); // Accessing mode from context
@@ -15,29 +15,27 @@ const CustomTable = (props) => {
         isLoading={props?.isLoading}
         options={{
           search: props?.search || true,
-          padding: "dense",
-          margin: 50,
+          tableLayout: 'fixed',
+          padding: 'dense',
+
           pageSize: props?.pageSize || 10,
           emptyRowsWhenPaging: props?.emptyRowsWhenPaging || false,
           exportButton: props?.exportButton || false,
           headerStyle: {
             backgroundColor: palette?.primary?.main,
-            color: "#FFF",
-            fontSize: ".8rem",
-            fontWeight:'bold',
-            padding: "dense",
-            height: 50,
-            textAlign: "center",
-            border: "2px solid #fff",
-            minHeight: "10px",
-            textTransform: "capitalize",
+            color: '#FFF',
+            fontSize: '.8rem',
+            fontWeight: 'bold',
+
+            textAlign: 'center',
+            border: '1px solid #fff',
+
+            textTransform: 'capitalize',
           },
-          rowStyle: {
-            fontSize: ".8rem",
-          },
+
           actionsColumnIndex: -1,
           rowStyle: props?.rowStyle || {
-            fontSize: ".8rem",
+            fontSize: '.8rem',
           },
         }}
         actions={props?.actions}
