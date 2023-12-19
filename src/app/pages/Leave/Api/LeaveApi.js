2b5a0bb90cdafc8dd537a272eca9c94254a38da1
bivek.joshi <bivek.joshi@dghub.io>
2023-12-19 11:26:18 +0545

    changes

--- src/app/pages/Leave/Api/LeaveApi.js
+++ src/app/pages/Leave/Api/LeaveApi.js
@@ -1,12 +1,12 @@
 import { useQuery } from "react-query";
 import { axiosInstance } from "../../../../auth/axiosInterceptor";
 
-const fetchDashBoard = () => {
+const fetchLeaveData = () => {
   return axiosInstance.get("/leave/leave-detail");
 };
 
 export const useLeaveDataSearch = (onSuccess, onError) => {
-  return useQuery(["leave-information"], () => fetchDashBoard(), {
+  return useQuery(["leave-information"], () => fetchLeaveData(), {
     onSuccess,
     onError,
     select: (response) => {
