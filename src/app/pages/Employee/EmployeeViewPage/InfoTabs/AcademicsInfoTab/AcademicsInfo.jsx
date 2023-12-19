6cbe25726ab722da4b524e5141a65a532b445c65
sanjay.thakur <sanjay.thakur@dghub.io>
2023-12-19 12:32:00 +0545

    fixes

--- src/app/pages/Employee/EmployeeViewPage/InfoTabs/AcademicsInfoTab/AcademicsInfo.jsx
+++ src/app/pages/Employee/EmployeeViewPage/InfoTabs/AcademicsInfoTab/AcademicsInfo.jsx
@@ -1,18 +1,22 @@
-import React, { useContext } from "react";
-import Timeline from "@mui/lab/Timeline";
-import { timelineItemClasses } from "@mui/lab/TimelineItem";
-import { Box } from "@mui/material";
-import AcademicsComponents from "./AcademicsComponents";
-import "../../EmployProfile/Style/Style.css";
-import ThemeModeContext from "../../../../../../theme/ThemeModeContext";
+import React, { useContext } from 'react';
+import Timeline from '@mui/lab/Timeline';
+import { timelineItemClasses } from '@mui/lab/TimelineItem';
+import { Box } from '@mui/material';
+import AcademicsComponents from './AcademicsComponents';
+import '../../EmployProfile/Style/Style.css';
+import ThemeModeContext from '../../../../../../theme/ThemeModeContext';
+import { useGetQualificationById } from '../../../../../hooks/employee/useQualification';
+import { useSelector } from 'react-redux';
 
-const AcademicsInfo = ({ data }) => {
+const AcademicsInfo = ({ data: userDetails }) => {
+  const { data, isLoading } = useGetQualificationById(userDetails?.id);
+
   const { mode } = useContext(ThemeModeContext);
   return (
     <Box
-      className="profileBasic"
+      className='profileBasic'
       sx={{
-        bgcolor: mode === "light" ? "#ededed" : "#454546",
+        bgcolor: mode === 'light' ? '#ededed' : '#454546',
       }}
     >
       <Timeline
@@ -21,16 +25,16 @@ const AcademicsInfo = ({ data }) => {
             flex: 0,
             padding: 0,
           },
-          marginTop: "0rem",
+          marginTop: '0rem',
         }}
       >
         {data
-          ? data.qualifications
+          ? data
               .sort((a, b) => b.passedYear - a.passedYear)
               .map((item, index) => (
                 <AcademicsComponents key={index} data={item} />
               ))
-          : ""}
+          : ''}
       </Timeline>
     </Box>
   );
