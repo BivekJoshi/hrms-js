95eb3f52587ac0cf7cd50be39dfcb67e71907ffb
sanjay.thakur <sanjay.thakur@dghub.io>
2023-12-20 10:04:47 +0545

    validation

--- src/app/hooks/employee/AddQualification/QualificationSchema.js
+++ src/app/hooks/employee/AddQualification/QualificationSchema.js
@@ -2,16 +2,16 @@ import * as Yup from 'yup';
 
 const QualificationSchema = Yup.object().shape({
   education: Yup.array()
-  .of(
+    .of(
       Yup.object().shape({
-        board: Yup.string().required("Board name is required"),
-        institute: Yup.string().required("Institude is reduied"),
-        passedLevel: Yup.string().required("Enter Pass Level Please"),
-        passedYear: Yup.string().required("Please Enter Passed Year"),
-        grade: Yup.string().required("Grade is required"),
+        board: Yup.string().required('Board name is required'),
+        institute: Yup.string().required('Institude is required'),
+        passedLevel: Yup.string().required('Enter Pass Level Please'),
+        passedYear: Yup.string().required('Please Enter Passed Year'),
+        grade: Yup.string().required('Grade is required'),
       })
-  )
-  .min(1, "Need at least a family")
+    )
+    .min(1, 'Need at least a family'),
 });
 
 export { QualificationSchema };
