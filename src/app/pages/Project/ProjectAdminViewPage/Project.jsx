51ad3ea2a17128d6a927b4eebedb2c32b7e8ec0e
Sujana Amgain <sujana.amgain@dghub.io>
2023-12-20 14:44:59 +0545

    document button

--- src/app/pages/Project/ProjectAdminViewPage/Project.jsx
+++ src/app/pages/Project/ProjectAdminViewPage/Project.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import Typography from '@mui/material/Typography';
+import React, { useState } from "react";
+import Typography from "@mui/material/Typography";
 import {
   Box,
   Button,
@@ -10,32 +10,30 @@ import {
   Pagination,
   Stack,
   TextField,
-} from '@mui/material';
-import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
+} from "@mui/material";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
 import {
   useGetProject,
   useGetProjectDetail,
   useGetProjectPageWise,
-} from '../../../hooks/project/useProject';
-import { AddProjectModal } from '../ProjectModal/ProjectModal';
+} from "../../../hooks/project/useProject";
+import { AddProjectModal } from "../ProjectModal/ProjectModal";
 
-import ProjectCard from '../../../components/cards/Employee/ProjectCard';
-import HocButton from '../../../hoc/hocButton';
-import PermissionHoc from '../../../hoc/permissionHoc';
-import useAuth from '../../../../auth/hooks/component/login/useAuth';
-import DeactivatedProject from '../DeactivatedProject/DeactivatedProject';
-import { ButtonComponent } from '../../../components/Button/ButtonComponent';
+import ProjectCard from "../../../components/cards/Employee/ProjectCard";
+import HocButton from "../../../hoc/hocButton";
+import PermissionHoc from "../../../hoc/permissionHoc";
+import useAuth from "../../../../auth/hooks/component/login/useAuth";
+import DeactivatedProject from "../DeactivatedProject/DeactivatedProject";
+import { ButtonComponent } from "../../../components/Button/ButtonComponent";
 
 const Project = ({ permissions }) => {
   const { isEmployee } = useAuth();
 
   const [openModal, setOpenModal] = useState(false);
-  const { data: projectData, isLoading } = useGetProject();
-  const { data: projectDetail } = useGetProjectDetail();
-console.log(projectDetail);
+  const { data: projectDetail, isLoading } = useGetProjectDetail();
 
-  const [nameFilter, setNameFilter] = useState('');
-  const [companyFilter, setCompanyFilter] = useState('');
+  const [nameFilter, setNameFilter] = useState("");
+  const [companyFilter, setCompanyFilter] = useState("");
   const [isContainerVisible, setIsContainerVisible] = useState(false);
   const [openAddModal, setOpenAddModal] = useState(false);
 
@@ -54,7 +52,7 @@ console.log(projectDetail);
     setOpenModal(false);
   };
 
-  const filteredProject = projectData?.filter((project) =>
+  const filteredProject = projectDetail?.filter((project) =>
     project?.projectName.toLowerCase().includes(nameFilter.toLowerCase())
   );
 
@@ -64,58 +62,50 @@ console.log(projectDetail);
     <>
       <Box>
         <Typography
-          className='project-button'
-          variant='h5'
+          className="project-button"
+          variant="h5"
           sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            marginBottom: '1.2rem',
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "1.2rem",
           }}
         >
           On-Going Projects
           {isEmployee ? null : (
-            <Box display='flex'>
+            <Box display="flex">
               <ButtonComponent
-                BGColor='white'
-                TextColor='#000'
+                BGColor="white"
+                TextColor="#000"
                 OnClick={handleOpenModal}
-                buttonName={'Terminated Project'}
+                buttonName={"Terminated Project"}
               />
               <ButtonComponent
-                color='white'
+                color="white"
                 OnClick={handleAddOpenModal}
-                buttonName='+ Add Project'
+                buttonName="+ Add Project"
               />
             </Box>
           )}
         </Typography>
       </Box>
 
-      <Stack sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
+      <Stack sx={{ display: "flex", flexDirection: "row-reverse" }}>
         <FilterAltOutlinedIcon
           onClick={handleFilterIconClick}
-          style={{ fontSize: '32px' }}
+          style={{ fontSize: "32px" }}
         />
         {isContainerVisible && (
-          <Container maxWidth='100vh'>
+          <Container maxWidth="100vh">
             <Card sx={{ padding: 1 }}>
-              <Typography variant='h6' gutterBottom>
+              <Typography variant="h6" gutterBottom>
                 Search Project
               </Typography>
               <Grid container spacing={3}>
-                <Grid item xs={12} sm={12} md={6}>
+                <Grid item xs={12} >
                   <TextField
-                    label='Filter by Name'
+                    label="Filter by Name"
                     value={nameFilter}
                     onChange={(e) => setNameFilter(e.target.value)}
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12} sm={12} md={6}>
-                  <TextField
-                    label='Filter by Company'
-                    value={companyFilter}
-                    onChange={(e) => setCompanyFilter(e.target.value)}
                     fullWidth
                   />
                 </Grid>
@@ -129,25 +119,26 @@ console.log(projectDetail);
         container
         item
         gap={3}
-        className='project-card-control'
+        className="project-card-control"
         sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(350px, 1fr))',
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
         }}
       >
-        {projectDetail?.map((item, index) => (
-          <ProjectCard
-            item={item}
-            Id={item.id}
-            key={index}
-            ProjectName={item.projectName}
-            StartDate={item.startDate}
-            EndDate={item.endDate}
-            ProjectLeaderId={item.projectLeadName}
-            TaskStatus={item.taskStatus}
-            totalEmployee={item.totalEmployee}
-          />
-        ))}
+        {filteredProject &&
+          filteredProject?.map((item, index) => (
+            <ProjectCard
+              item={item}
+              Id={item.id}
+              key={index}
+              ProjectName={item.projectName}
+              StartDate={item.startDate}
+              EndDate={item.endDate}
+              ProjectLeaderId={item.projectLeadName}
+              TaskStatus={item.taskStatus}
+              totalEmployee={item.totalEmployee}
+            />
+          ))}
       </Grid>
 
       {/* <Box padding="2rem" display="grid" justifyContent={"center"}>
@@ -163,7 +154,7 @@ console.log(projectDetail);
 
       {openAddModal && (
         <AddProjectModal
-          title={'Add Project'}
+          title={"Add Project"}
           open={openAddModal}
           handleCloseModal={handleCloseAddModal}
         />
@@ -172,17 +163,17 @@ console.log(projectDetail);
       <Modal
         open={openModal}
         onClose={handleCloseModal}
-        aria-labelledby='modal-title'
-        aria-describedby='modal-description'
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
       >
         <Box
           sx={{
-            position: 'absolute',
-            top: '40%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
+            position: "absolute",
+            top: "40%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
             // width: 400,
-            bgcolor: 'background.paper',
+            bgcolor: "background.paper",
             boxShadow: 24,
             p: 4,
           }}
@@ -191,14 +182,14 @@ console.log(projectDetail);
           <br />
           <Grid
             container
-            direction='row'
-            justifyContent='flex-end'
-            alignItems='flex-end'
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="flex-end"
           >
             <Button
               onClick={() => setOpenModal(false)}
-              color='error'
-              variant='contained'
+              color="error"
+              variant="contained"
             >
               Close
             </Button>
