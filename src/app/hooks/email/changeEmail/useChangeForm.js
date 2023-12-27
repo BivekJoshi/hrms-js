a001c2e7c28364444bebdcfce10a0fc3894c8dc4
sanjay.thakur <sanjay.thakur@dghub.io>
2023-12-27 15:07:01 +0545

    doc preview added

--- src/app/hooks/email/changeEmail/useChangeForm.js
+++ src/app/hooks/email/changeEmail/useChangeForm.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { useChangeEmail } from '../useEmail';
 import { useFormik } from 'formik';
 import changeEmailSchema from './changeEmailSchema';
+import { removeUser } from '../../../utils/cookieHelper';
+import { useNavigate } from 'react-router-dom';
 
 const useChangeForm = () => {
   const { mutate: changeEmail } = useChangeEmail({});
+  const navigate = useNavigate();
   const formik = useFormik({
     initialValues: {
       newEmail: '',
@@ -20,6 +23,8 @@ const useChangeForm = () => {
     const values = { newEmail };
     changeEmail(values, formik, {
       onSuccess: () => {
+        removeUser();
+        navigate('/');
         formik.resetForm();
       },
     });
