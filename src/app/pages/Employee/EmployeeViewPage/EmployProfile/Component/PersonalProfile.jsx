5ccce47c146a2b9a210c0fde9eaa2ae23a8c6b20
sanjay.thakur <sanjay.thakur@dghub.io>
2023-12-28 15:12:29 +0545

    gg

--- src/app/pages/Employee/EmployeeViewPage/EmployProfile/Component/PersonalProfile.jsx
+++ src/app/pages/Employee/EmployeeViewPage/EmployProfile/Component/PersonalProfile.jsx
@@ -1,21 +1,21 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState } from 'react';
 
-import { Box, Button, Chip, Grid } from "@mui/material";
-import { Typography, Avatar } from "@mui/material";
+import { Box, Button, Chip, Grid } from '@mui/material';
+import { Typography, Avatar } from '@mui/material';
 
-import Male from "../../../../../../assets/male.png";
-import Female from "../../../../../../assets/female.png";
-import BasicInfo from "./BasicInfo";
-import ThemeModeContext from "../../../../../../theme/ThemeModeContext";
-import { DOC_URL } from "../../../../../../auth/axiosInterceptor";
-import EmailModal from "../../../../Email/EmailModal";
-import { useGetEmployeeByDesignation } from "../../../../../hooks/employee/useEmployee";
-import { useGetDesignationById } from "../../../../../hooks/designation/useDesignation";
-import { useParams } from "react-router-dom";
-import { useGetLoggedInUser } from "../../../../../hooks/auth/usePassword";
-import useAuth from "../../../../../../auth/hooks/component/login/useAuth";
+import Male from '../../../../../../assets/male.png';
+import Female from '../../../../../../assets/female.png';
+import BasicInfo from './BasicInfo';
+import ThemeModeContext from '../../../../../../theme/ThemeModeContext';
+import { DOC_URL } from '../../../../../../auth/axiosInterceptor';
+import EmailModal from '../../../../Email/EmailModal';
+import { useGetEmployeeByDesignation } from '../../../../../hooks/employee/useEmployee';
+import { useGetDesignationById } from '../../../../../hooks/designation/useDesignation';
+import { useParams } from 'react-router-dom';
+import { useGetLoggedInUser } from '../../../../../hooks/auth/usePassword';
+import useAuth from '../../../../../../auth/hooks/component/login/useAuth';
 
-const primaryColor = "#1c7ed6";
+const primaryColor = '#1c7ed6';
 
 export const PersonalProfile = ({ data }) => {
   const { isEmployee } = useAuth();
@@ -35,91 +35,90 @@ export const PersonalProfile = ({ data }) => {
   const employeeImg = loggedUserData?.userPhotoPath;
   const employeeFilePath = employeeImg
     ? DOC_URL + employeeImg
-    : data?.gender === "MALE"
+    : data?.gender === 'MALE'
     ? Male
     : Female;
 
   const photo = data?.employeePhotoPath;
   const filePath = photo
     ? DOC_URL + photo
-    : data?.gender === "MALE"
+    : data?.gender === 'MALE'
     ? Male
     : Female;
 
-    console.log(positionData);
   return (
     <>
       <Grid
         sx={{
-          bgcolor: mode === "light" ? "#cfe8fc" : "#292929",
-          display: "grid",
-          gridTemplateColumns: "1fr 3fr",
+          bgcolor: mode === 'light' ? '#cfe8fc' : '#292929',
+          display: 'grid',
+          gridTemplateColumns: '1fr 3fr',
           borderRadius: 5,
-          alignItems: "center",
-          p: "1rem",
-          gap: "1rem",
+          alignItems: 'center',
+          p: '1rem',
+          gap: '1rem',
         }}
       >
         <Box
-          className="profileInfo"
-          bgcolor={mode === "light" ? "" : "#3f413f"}
-          sx={{ background: mode === "light" ? "#bfddf5" : "#3f413f" }}
+          className='profileInfo'
+          bgcolor={mode === 'light' ? '' : '#3f413f'}
+          sx={{ background: mode === 'light' ? '#bfddf5' : '#3f413f' }}
         >
           <Avatar
             sx={{
               width: 190,
               height: 190,
               bgcolor: primaryColor,
-              alignSelf: "center",
+              alignSelf: 'center',
             }}
-            variant="circle"
+            variant='circle'
             src={isEmployee ? employeeFilePath : filePath}
           />
           <Typography
             sx={{
               color: primaryColor,
-              fontSize: "1.3rem",
-              fontWeight: "600",
+              fontSize: '1.3rem',
+              fontWeight: '600',
             }}
           >
-            {data?.firstName + " " + data?.middleName + " " + data?.lastName}
+            {data?.firstName + ' ' + data?.middleName + ' ' + data?.lastName}
           </Typography>
           <Chip
             label={positionName}
-            sx={{ bgcolor: primaryColor, color: "white", width: " 9rem" }}
+            sx={{ bgcolor: primaryColor, color: 'white', width: ' 9rem' }}
           />
           <Typography
             onClick={handleOpenEmailform}
             sx={{
               color: primaryColor,
-              fontSize: "1rem",
-              fontWeight: "600",
-              cursor: "pointer",
+              fontSize: '1rem',
+              fontWeight: '600',
+              cursor: 'pointer',
             }}
           >
             {data?.officeEmail}
           </Typography>
           <Typography
-            sx={{ color: primaryColor, fontSize: "1rem", fontWeight: "600" }}
+            sx={{ color: primaryColor, fontSize: '1rem', fontWeight: '600' }}
           >
             {data?.data?.addresses[0]?.city === null
-              ? ""
+              ? ''
               : data?.data?.addresses[0]?.city}
           </Typography>
           <Typography
-            sx={{ color: primaryColor, fontSize: "1rem", fontWeight: "600" }}
+            sx={{ color: primaryColor, fontSize: '1rem', fontWeight: '600' }}
           >
             {data?.mobileNumber}
           </Typography>
         </Box>
 
-        <BasicInfo data={data} mode={mode} positionName={positionName}/>
+        <BasicInfo data={data} mode={mode} positionName={positionName} />
       </Grid>
 
       {openEmailForm && (
         <EmailModal
-          title={"Send Email"}
-          officeEmail={data?.officeEmail || ""}
+          title={'Send Email'}
+          officeEmail={data?.officeEmail || ''}
           employeeId={data?.id}
           open={openEmailForm}
           onClose={handleCloseEmailform}
