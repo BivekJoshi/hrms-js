b490408dda1ddd27def617baca5439eb87909b3a
bivek.joshi <bivek.joshi@dghub.io>
2023-12-31 15:09:33 +0545

    changes

--- src/auth/getBaseUrl.js
+++ src/auth/getBaseUrl.js
@@ -9,12 +9,12 @@ export const contextPath = () => {
 export const getBaseUrl = () => {
   if (envType === "development") {
     // return 'http://172.16.16.94:8083/hrms/api/';
-    return 'http://172.16.16.85:8083/hrms/api/';
+    // return 'http://172.16.16.85:8083/hrms/api/';
     // return 'http://172.16.16.190:8083/hrms/api/'; //Ajit sir
 
     // return "http://103.94.159.144:8083/hrms/api/";
 
-    // return 'https://dgtrade.dghub.io:8080/hrms';
+    return 'https://dgtrade.dghub.io:8080/hrms';
   } else if (envType === "production") {
     return contextPath() + "/api";
   }
