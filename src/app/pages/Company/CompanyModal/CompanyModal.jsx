1616e4eb2de17884eab2b44ffff7985a82622d92
ganesh bhatt <@ganesh.bhatta>
2024-01-02 16:38:01 +0545

    branch department added fix

--- src/app/pages/Company/CompanyModal/CompanyModal.jsx
+++ src/app/pages/Company/CompanyModal/CompanyModal.jsx
@@ -3,7 +3,6 @@ import FormModal from "../../../components/Modal/FormModal";
 import { useGetCompanyById } from "../../../hooks/company/useCompany";
 import CompanyFields from "../../../components/Form/Company/CompanyFields";
 import { Box } from "@mui/material";
-import AssignCompanyFields from '../../../components/Form/Company/AssignCompanyFields';
 
 export const AddCompanyModal = ({ open, handleCloseModal, title }) => {
   return (
@@ -27,20 +26,6 @@ export const EditCompanyModal = ({ open, handleCloseModal, data, title }) => {
         open={open}
         onClose={handleCloseModal}
         formComponent={<CompanyFields onClose={handleCloseModal} data={data} />}
-      />
-    </Box>
-  );
-};
-
-export const EditAssignCompanyModal = ({ open, handleCloseModal, data, title }) => {
-  // const { data } = useGetCompanyById(id);
-  return (
-    <Box>
-      <FormModal
-        title={title}
-        open={open}
-        onClose={handleCloseModal}
-        formComponent={<AssignCompanyFields onClose={handleCloseModal} data={data} />}
       />
     </Box>
   );
