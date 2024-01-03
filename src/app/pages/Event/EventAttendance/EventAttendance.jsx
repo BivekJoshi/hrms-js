ed2b84cb56bcdbd3d123db77988f0e1e3e7abbd1
sanjay.thakur <sanjay.thakur@dghub.io>
2024-01-03 16:07:20 +0545

    merge

--- src/app/pages/Event/EventAttendance/EventAttendance.jsx
+++ src/app/pages/Event/EventAttendance/EventAttendance.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
-import { useGetEventAttenderList } from "../../../hooks/event/useEvent";
-import PermissionHoc from "../../../hoc/permissionHoc";
-import HocButton from "../../../hoc/hocButton";
-import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
-import CustomTable from "../../../components/CustomTable/CustomTable";
-import DoneSharpIcon from "@mui/icons-material/DoneSharp";
-import CloseSharpIcon from "@mui/icons-material/CloseSharp";
-import { EditEventAttendanceModal } from "../EventModal/EventModal";
-import NewFilter from "../../../components/NewFilter/NewFilter";
-import { useGetAllEvent, usegetAllEmployeeData } from "./useEventAttendance";
-import { Badge, Chip, Typography } from "@mui/material";
+import React, { useEffect, useState } from 'react';
+import { useGetEventAttenderList } from '../../../hooks/event/useEvent';
+import PermissionHoc from '../../../hoc/permissionHoc';
+import HocButton from '../../../hoc/hocButton';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+import CustomTable from '../../../components/CustomTable/CustomTable';
+import DoneSharpIcon from '@mui/icons-material/DoneSharp';
+import CloseSharpIcon from '@mui/icons-material/CloseSharp';
+import { EditEventAttendanceModal } from '../EventModal/EventModal';
+import NewFilter from '../../../components/NewFilter/NewFilter';
+import { useGetAllEvent, usegetAllEmployeeData } from './useEventAttendance';
+import { Badge, Chip, Typography } from '@mui/material';
+import { getEventAttenderList } from '../../../api/event/event-api';
+import { toast } from 'react-toastify';
 
 const EventAttendance = ({ permissions }) => {
   const { employeeData } = usegetAllEmployeeData();
@@ -17,9 +19,15 @@ const EventAttendance = ({ permissions }) => {
   const [openEditModal, setOpenEditModal] = useState(false);
   const [editedEventAttendance, setEditedEventAttendance] = useState({});
   const handleCloseEditModal = () => setOpenEditModal(false);
-  const [filterState, setFilterState] = useState("");
-  const { data: eventAttendanceData, isLoading } =
-    useGetEventAttenderList(filterState);
+  const [filterState, setFilterState] = useState('');
+
+  const [tableData, setTableData] = useState([]);
+  console.log(
+    '🚀 ~ file: EventAttendance.jsx:25 ~ EventAttendance ~ tableData:',
+    tableData
+  );
+  // const { data: eventAttendanceData, isLoading } =
+  //   useGetEventAttenderList(filterState);
 
   const handleEditEventAttendance = (rowData) => {
     setEditedEventAttendance(rowData);
@@ -28,18 +36,18 @@ const EventAttendance = ({ permissions }) => {
 
   const filterMenu = [
     {
-      label: "Event",
-      name: "eventId",
-      type: "autoComplete",
+      label: 'Event',
+      name: 'eventId',
+      type: 'autoComplete',
       options: eventData || [],
       md: 6,
       sm: 6,
       xs: 6,
     },
     {
-      label: "Employee",
-      name: "employeeId",
-      type: "autoComplete",
+      label: 'Employee',
+      name: 'employeeId',
+      type: 'autoComplete',
       options: employeeData || [],
       md: 6,
       sm: 6,
@@ -49,32 +57,32 @@ const EventAttendance = ({ permissions }) => {
 
   const columns = [
     {
-      title: "SN",
+      title: 'SN',
       render: (rowData) => rowData.tableData.id + 1,
-      width: "3%",
+      width: '3%',
       sortable: false,
       sorting: false,
     },
 
     {
-      title: "Employee Name",
-      field: "userName",
-      emptyValue: "-",
-      width: "20vh",
+      title: 'Employee Name',
+      field: 'userName',
+      emptyValue: '-',
+      width: '20vh',
       sorting: false,
     },
     {
-      title: "Branch",
-      field: "branch",
-      emptyValue: "-",
+      title: 'Branch',
+      field: 'branch',
+      emptyValue: '-',
       sortable: false,
       sorting: false,
     },
     {
-      title: "Contact Detail",
-      field: "mobileNumber",
-      emptyValue: "-",
-      width: "20vh",
+      title: 'Contact Detail',
+      field: 'mobileNumber',
+      emptyValue: '-',
+      width: '20vh',
       sorting: false,
       render: (rowData) => {
         return (
@@ -86,86 +94,86 @@ const EventAttendance = ({ permissions }) => {
       },
     },
     {
-      title: "Emial",
-      field: "email",
+      title: 'Email',
+      field: 'email',
       hidden: true,
       export: true,
     },
     {
-      title: "Event Name",
-      field: "eventName",
-      emptyValue: "-",
-      width: "20vh",
+      title: 'Event Name',
+      field: 'eventName',
+      emptyValue: '-',
+      width: '20vh',
       sorting: false,
     },
     {
-      title: "Date",
-      field: "eventDate",
-      emptyValue: "-",
-      width: "20vh",
+      title: 'Date',
+      field: 'eventDate',
+      emptyValue: '-',
+      width: '20vh',
       sorting: false,
     },
     {
-      title: "Time",
-      field: "eventTime",
-      emptyValue: "-",
-      width: "20vh",
+      title: 'Time',
+      field: 'eventTime',
+      emptyValue: '-',
+      width: '20vh',
       sorting: false,
     },
     {
-      title: "Location",
-      field: "eventLocation",
-      emptyValue: "-",
-      width: "20vh",
+      title: 'Location',
+      field: 'eventLocation',
+      emptyValue: '-',
+      width: '20vh',
       sorting: false,
     },
     {
-      title: "Description",
-      field: "eventDescription",
-      emptyValue: "-",
-      width: "20vh",
+      title: 'Description',
+      field: 'eventDescription',
+      emptyValue: '-',
+      width: '20vh',
       sorting: false,
     },
     {
-      title: "User Confirmation",
-      field: "status",
-      emptyValue: "-",
+      title: 'User Confirmation',
+      field: 'status',
+      emptyValue: '-',
       sorting: false,
-      align: "center",
+      align: 'center',
       render: (rowData) => {
-        if (rowData?.status === "OK") {
-          return <Chip color="success" label="Coming" />;
+        if (rowData?.status === 'OK') {
+          return <Chip color='success' label='Coming' />;
         } else
           return (
             <Chip
-              color="error"
-              sx={{ width: "max-content" }}
-              label="Not Coming"
+              color='error'
+              sx={{ width: 'max-content' }}
+              label='Not Coming'
             />
           );
       },
     },
     {
-      title: "Attended",
-      field: "isPresent",
+      title: 'Attended',
+      field: 'isPresent',
       render: (rowData) => {
         if (rowData?.isPresent) {
           return (
             <div>
-              <Badge color="success" badgeContent="Yes" />
+              <Badge color='success' badgeContent='Yes' />
             </div>
           );
         } else
           return (
             <div>
-              <Badge color="error" badgeContent="No" />
+              <Badge color='error' badgeContent='No' />
             </div>
           );
       },
-      emptyValue: "-",
-      width: "20vh",
+      emptyValue: '-',
+      width: '20vh',
       sorting: false,
-      align: "center",
+      align: 'center',
     },
   ].filter(Boolean);
 
@@ -177,36 +185,44 @@ const EventAttendance = ({ permissions }) => {
           icon={<ModeEditOutlineIcon />}
         />
       ),
-      tooltip: "Edit Event",
+      tooltip: 'Edit Event',
       onClick: (event, rowData) => handleEditEventAttendance(rowData),
     },
   ];
 
-  const handleSearch = (values) => {
-    setFilterState({ ...filterState, ...values });
+  useEffect(() => {
+    handleSearch();
+  }, []);
+
+  const handleSearch = async (values) => {
+    try {
+      const data = await getEventAttenderList({ ...values });
+
+      setTableData(data?.events);
+    } catch (error) {
+      toast.error(error);
+    }
   };
   return (
-    !isLoading && (
-      <>
-        <NewFilter inputField={filterMenu} searchCallBack={handleSearch} />
-        <CustomTable
-          columns={columns}
-          data={eventAttendanceData?.events}
-          title="Event Attendance Data"
-          isLoading={isLoading}
-          actions={actions}
-          exportButton={filterState?.eventId ? true : false}
+    <>
+      <NewFilter inputField={filterMenu} searchCallBack={handleSearch} />
+      <CustomTable
+        columns={columns}
+        data={tableData}
+        title='Event Attendance Data'
+        // isLoading={isLoading}
+        actions={actions}
+        exportButton={filterState?.eventId ? true : false}
+      />
+      {openEditModal && (
+        <EditEventAttendanceModal
+          title={'Edit Event Attendance'}
+          data={editedEventAttendance}
+          open={openEditModal}
+          handleCloseModal={handleCloseEditModal}
         />
-        {openEditModal && (
-          <EditEventAttendanceModal
-            title={"Edit Event Attendance"}
-            data={editedEventAttendance}
-            open={openEditModal}
-            handleCloseModal={handleCloseEditModal}
-          />
-        )}
-      </>
-    )
+      )}
+    </>
   );
 };
 
