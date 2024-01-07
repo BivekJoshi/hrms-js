4cdff8ba5772fbc7f59eaab20b4d8b9561d78e03
ganesh bhatt <@ganesh.bhatta>
2024-01-07 12:31:31 +0545

    role wise permission changed

--- src/auth/getBaseUrl.js
+++ src/auth/getBaseUrl.js
@@ -10,10 +10,10 @@ export const getBaseUrl = () => {
   if (envType === 'development') {
     // return 'http://172.16.16.94:8083/hrms/api/';
     // return 'http://172.16.16.85:8083/hrms/api/';
-    return 'http://172.16.16.53:8083/hrms/api/'; // bipeen sir
+    // return 'http://172.16.16.53:8083/hrms/api/'; // bipeen sir
     // return 'http://bipeen:8083/hrms/api/'; //Ajit sir
 
-    // return "http://103.94.159.144:8083/hrms/api/";
+    return "http://103.94.159.144:8083/hrms/api/";
 
     // return 'https://dgtrade.dghub.io:8080/hrms';
   } else if (envType === 'production') {
