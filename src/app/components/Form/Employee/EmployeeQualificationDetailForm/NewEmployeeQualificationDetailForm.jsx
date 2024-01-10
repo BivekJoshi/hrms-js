757b1b11e4c83d7d635c09423d3f21492f812565
sanjay.thakur <sanjay.thakur@dghub.io>
2024-01-10 17:24:16 +0545

    gg

--- src/app/components/Form/Employee/EmployeeQualificationDetailForm/NewEmployeeQualificationDetailForm.jsx
+++ src/app/components/Form/Employee/EmployeeQualificationDetailForm/NewEmployeeQualificationDetailForm.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-import QualificationAddField from "./QualificationAddField";
-import useAddQualificationDetails from "./useAddQualificationDetail";
-import CustomeEmployeeDetails from "../../../../utils/CustomeDetails/CustomeEmployeeDetails";
+import QualificationAddField from './QualificationAddField';
+import useAddQualificationDetails from './useAddQualificationDetail';
+import CustomeEmployeeDetails from '../../../../utils/CustomeDetails/CustomeEmployeeDetails';
 import {
   useDeleteQualification,
   useGetQualificationById,
-} from "../../../../hooks/employee/useQualification";
-import { TableCell } from "@material-ui/core";
-import { Preview } from "@mui/icons-material";
-import { TableRow } from "@mui/material";
-import { DOC_URL } from "../../../../../auth/axiosInterceptor";
+} from '../../../../hooks/employee/useQualification';
+import { TableCell } from '@material-ui/core';
+import { Preview } from '@mui/icons-material';
+import { TableRow } from '@mui/material';
+import { DOC_URL } from '../../../../../auth/axiosInterceptor';
 
 const NewEmployeeQualificationDetailForm = () => {
   const { id } = useParams();
@@ -31,21 +31,21 @@ const NewEmployeeQualificationDetailForm = () => {
   };
 
   const columns = [
-    { id: "board", label: "Board", minWidth: 170 },
-    { id: "institute", label: "Institude", minWidth: 150 },
+    { id: 'board', label: 'Board', minWidth: 170 },
+    { id: 'institute', label: 'Institude', minWidth: 150 },
     {
-      id: "passedLevel",
-      label: "Passed Level",
+      id: 'passedLevel',
+      label: 'Passed Level',
       minWidth: 150,
     },
-    { id: "passedYear", label: "Passed Year", minWidth: 150 },
-    { id: "scoreType", label: "Score Type", minWidth: 150 },
-    { id: "grade", label: "Score", minWidth: 150 },
+    { id: 'passedYear', label: 'Passed Year', minWidth: 150 },
+    { id: 'scoreType', label: 'Score Type', minWidth: 150 },
+    { id: 'grade', label: 'Score', minWidth: 150 },
     {
-      id: "actions",
-      label: "Actions",
+      id: 'actions',
+      label: 'Actions',
       minWidth: 50,
-      align: "right",
+      align: 'right',
     },
   ];
 
@@ -53,7 +53,7 @@ const NewEmployeeQualificationDetailForm = () => {
     <div>
       <CustomeEmployeeDetails
         formik={formik}
-        title={"Education Detail"}
+        title={'Education Detail'}
         columns={columns}
         data={data}
         renderFeilds={<QualificationAddField formik={formik} />}
@@ -63,48 +63,6 @@ const NewEmployeeQualificationDetailForm = () => {
         deleteCallBack={handleDeleteHistory}
         showDocumentImg
         modalWidth={480}
-        renderShowDocumentCell={
-          <TableRow>
-            {data?.map((d) => {
-              const {
-                transcriptPath,
-                otherDocumentPath,
-                characterCertificatePath,
-              } = d;
-              const documentName = transcriptPath
-                ? "Transcript"
-                : otherDocumentPath
-                ? "Other Document"
-                : characterCertificatePath
-                ? "Character Certificate"
-                : "";
-              if (
-                !transcriptPath &&
-                !otherDocumentPath &&
-                !characterCertificatePath
-              )
-                return <TableCell></TableCell>;
-              else {
-                return (
-                  <TableCell>
-                    {transcriptPath && (
-                      <ShowImage name={documentName} path={transcriptPath} />
-                    )}
-                    {characterCertificatePath && (
-                      <ShowImage
-                        name={documentName}
-                        path={characterCertificatePath}
-                      />
-                    )}
-                    {otherDocumentPath && (
-                      <ShowImage name={documentName} path={otherDocumentPath} />
-                    )}
-                  </TableCell>
-                );
-              }
-            })}
-          </TableRow>
-        }
       />
     </div>
   );
@@ -112,7 +70,7 @@ const NewEmployeeQualificationDetailForm = () => {
 
 const ShowImage = ({ name, path }) => {
   return (
-    <div style={{margin:"0px 5px"}}>
+    <div style={{ margin: '0px 5px' }}>
       {name} <img height={100} width={100} src={DOC_URL + path} alt={name} />
     </div>
   );
--- src/app/utils/CustomeDetails/CustomeEmployeeDetails.jsx
+++ src/app/utils/CustomeDetails/CustomeEmployeeDetails.jsx
@@ -14,14 +14,14 @@ import {
   TableHead,
   TableRow,
   Typography,
-} from "@mui/material";
-import React, { useContext, useEffect, useState } from "react";
-import AddFields from "./AddFields";
-import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
-import DeleteIcon from "@mui/icons-material/Delete";
-import Backdrop from "@mui/material/Backdrop";
-import ThemeModeContext from "../../../theme/ThemeModeContext";
-import CloseIcon from "@mui/icons-material/Close";
+} from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import AddFields from './AddFields';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Backdrop from '@mui/material/Backdrop';
+import ThemeModeContext from '../../../theme/ThemeModeContext';
+import CloseIcon from '@mui/icons-material/Close';
 
 const CustomeEmployeeDetails = ({
   formik,
@@ -43,18 +43,18 @@ const CustomeEmployeeDetails = ({
   const { mode } = useContext(ThemeModeContext);
 
   const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
     width: modalWidth && modalWidth,
-    bgcolor: "background.paper",
-    border: "1px solid #808080",
+    bgcolor: 'background.paper',
+    border: '1px solid #808080',
     borderRadius: 2,
     boxShadow: 24,
-    p: "12px 24px",
-    height: modalHeight ? modalHeight : "70vh",
-    overflowY: "auto",
+    p: '12px 24px',
+    height: modalHeight ? modalHeight : '70vh',
+    overflowY: 'auto',
   };
 
   useEffect(() => {
@@ -77,24 +77,24 @@ const CustomeEmployeeDetails = ({
   };
   return (
     <div>
-      <Typography variant="h6" textAlign="center">
+      <Typography variant='h6' textAlign='center'>
         {title}
       </Typography>
 
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <Button
-          variant="contained"
+          variant='contained'
           onClick={() => {
             setOpenAddModal(true);
           }}
-          sx={{ textTransform: "capitalize", mb: 1 }}
+          sx={{ textTransform: 'capitalize', mb: 1 }}
         >
           + Add
         </Button>
       </div>
-      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
         <TableContainer sx={{ maxHeight: 440 }}>
-          <Table stickyHeader aria-label="sticky table">
+          <Table stickyHeader aria-label='sticky table'>
             <TableHead>
               <TableRow>
                 {columns?.map((column) => (
@@ -115,30 +115,30 @@ const CustomeEmployeeDetails = ({
                     <>
                       <TableRow
                         hover
-                        role="checkbox"
+                        role='checkbox'
                         tabIndex={-1}
                         key={row.code}
                       >
                         {columns?.map((column) => {
                           const value = row[column.id];
-                          if (column?.id === "actions") {
+                          if (column?.id === 'actions') {
                             return (
                               <TableCell key={column.id} align={column.align}>
                                 <div
                                   style={{
-                                    display: "flex",
-                                    gap: "12px",
-                                    justifyContent: "center",
-                                    alignItems: "center",
+                                    display: 'flex',
+                                    gap: '12px',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
                                   }}
                                 >
                                   <ModeEditOutlineIcon
                                     sx={{
                                       color:
-                                        mode !== "dark" ? "black" : "#fcfcfc",
-                                      cursor: "pointer",
-                                      "&:hover": {
-                                        color: "green",
+                                        mode !== 'dark' ? 'black' : '#fcfcfc',
+                                      cursor: 'pointer',
+                                      '&:hover': {
+                                        color: 'green',
                                       },
                                     }}
                                     onClick={() => handleEdit(row)}
@@ -146,10 +146,10 @@ const CustomeEmployeeDetails = ({
                                   <DeleteIcon
                                     sx={{
                                       color:
-                                        mode !== "dark" ? "black" : "#fcfcfc",
-                                      cursor: "pointer",
-                                      "&:hover": {
-                                        color: "red",
+                                        mode !== 'dark' ? 'black' : '#fcfcfc',
+                                      cursor: 'pointer',
+                                      '&:hover': {
+                                        color: 'red',
                                       },
                                     }}
                                     onClick={() => deleteCallBack(row)}
@@ -166,14 +166,41 @@ const CustomeEmployeeDetails = ({
                           }
                         })}
                       </TableRow>
-                      {showDocumentImg && renderShowDocumentCell}
+                      {showDocumentImg && (
+                        <TableRow>
+                          {(() => {
+                            const {
+                              transcriptPath,
+                              otherDocumentPath,
+                              characterCertificatePath,
+                            } = row;
+                            const documentName = transcriptPath
+                              ? 'Transcript'
+                              : otherDocumentPath
+                              ? 'Other Document'
+                              : characterCertificatePath
+                              ? 'Character Certificate'
+                              : '';
+
+                            if (
+                              !transcriptPath &&
+                              !otherDocumentPath &&
+                              !characterCertificatePath
+                            ) {
+                              return <TableCell></TableCell>;
+                            } else {
+                              return <TableCell></TableCell>;
+                            }
+                          })()}
+                        </TableRow>
+                      )}
                     </>
                   );
                 })
               ) : (
                 <TableRow>
                   <TableCell></TableCell>
-                  <TableCell align="center">No Record Found</TableCell>
+                  <TableCell align='center'>No Record Found</TableCell>
                 </TableRow>
               )}
             </TableBody>
@@ -182,8 +209,8 @@ const CustomeEmployeeDetails = ({
       </Paper>
       {openAddModal && (
         <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
+          aria-labelledby='transition-modal-title'
+          aria-describedby='transition-modal-description'
           open={openAddModal}
           onClose={handleCloseAddModal}
           closeAfterTransition
@@ -198,26 +225,26 @@ const CustomeEmployeeDetails = ({
             <Box sx={style}>
               <Grid
                 sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  marginBottom: "1rem",
-                  position: "relative",
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  marginBottom: '1rem',
+                  position: 'relative',
                 }}
               >
                 <Typography
-                  variant="h6"
-                  color={mode === "dark" ? "#fcfcfc" : ""}
+                  variant='h6'
+                  color={mode === 'dark' ? '#fcfcfc' : ''}
                 >
-                  {selectedRowId ? "Edit" : "Add"} {title}
+                  {selectedRowId ? 'Edit' : 'Add'} {title}
                 </Typography>
                 <div
                   style={{
-                    width: "100%",
-                    height: "1px",
-                    backgroundColor: "#e0e0e0",
-                    position: "absolute",
-                    bottom: "0",
+                    width: '100%',
+                    height: '1px',
+                    backgroundColor: '#e0e0e0',
+                    position: 'absolute',
+                    bottom: '0',
                   }}
                 />
                 <IconButton onClick={handleCloseAddModal}>
