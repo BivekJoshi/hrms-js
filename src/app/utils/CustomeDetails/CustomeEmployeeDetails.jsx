c25a455b27d6146c76bf3abe156dd72991209279
sanjay.thakur <sanjay.thakur@dghub.io>
2024-01-11 11:03:58 +0545

    fix doc upload

--- src/app/utils/CustomeDetails/CustomeEmployeeDetails.jsx
+++ src/app/utils/CustomeDetails/CustomeEmployeeDetails.jsx
@@ -14,15 +14,15 @@ import {
   TableHead,
   TableRow,
   Typography,
-} from "@mui/material";
-import React, { useContext, useEffect, useState } from "react";
-import AddFields from "./AddFields";
-import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
-import DeleteIcon from "@mui/icons-material/Delete";
-import Backdrop from "@mui/material/Backdrop";
-import ThemeModeContext from "../../../theme/ThemeModeContext";
-import CloseIcon from "@mui/icons-material/Close";
-import ShowImagePreview from "./ShowImagePreview";
+} from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import AddFields from './AddFields';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Backdrop from '@mui/material/Backdrop';
+import ThemeModeContext from '../../../theme/ThemeModeContext';
+import CloseIcon from '@mui/icons-material/Close';
+import ShowImagePreview from './ShowImagePreview';
 
 const CustomeEmployeeDetails = ({
   formik,
@@ -41,20 +41,20 @@ const CustomeEmployeeDetails = ({
   const [openAddModal, setOpenAddModal] = useState(false);
   const [selectedRowId, setSelectedRowId] = useState();
   const { mode } = useContext(ThemeModeContext);
-
+  const [documentData, setDocumentData] = useState([]);
   const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
     width: modalWidth && modalWidth,
-    bgcolor: "background.paper",
-    border: "1px solid #808080",
+    bgcolor: 'background.paper',
+    border: '1px solid #808080',
     borderRadius: 2,
     boxShadow: 24,
-    p: "12px 24px",
+    p: '12px 24px',
     height: modalHeight && modalHeight,
-    overflowY: "auto",
+    overflowY: 'auto',
   };
 
   useEffect(() => {
@@ -75,26 +75,62 @@ const CustomeEmployeeDetails = ({
     setOpenAddModal(true);
     setSelectedRowId(row?.id);
   };
+
+  useEffect(() => {
+    const accumulateDocumentData = () => {
+      const newDocumentData = [];
+
+      data.forEach((row) => {
+        const { transcriptPath, otherDocumentPath, characterCertificatePath } =
+          row;
+
+        if (transcriptPath) {
+          newDocumentData.push({
+            name: 'Transcript',
+            path: transcriptPath,
+          });
+        }
+
+        if (otherDocumentPath) {
+          newDocumentData.push({
+            name: 'Other Document',
+            path: otherDocumentPath,
+          });
+        }
+
+        if (characterCertificatePath) {
+          newDocumentData.push({
+            name: 'Character Certificate',
+            path: characterCertificatePath,
+          });
+        }
+      });
+
+      setDocumentData(newDocumentData);
+    };
+
+    accumulateDocumentData();
+  }, [data]);
   return (
     <div>
-      <Typography variant="h6" textAlign="center">
+      <Typography variant='h6' textAlign='center'>
         {title}
       </Typography>
 
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <Button
-          variant="contained"
+          variant='contained'
           onClick={() => {
             setOpenAddModal(true);
           }}
-          sx={{ textTransform: "capitalize", mb: 1 }}
+          sx={{ textTransform: 'capitalize', mb: 1 }}
         >
           + Add
         </Button>
       </div>
-      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
         <TableContainer sx={{ maxHeight: 440 }}>
-          <Table stickyHeader aria-label="sticky table">
+          <Table stickyHeader aria-label='sticky table'>
             <TableHead>
               <TableRow>
                 {columns?.map((column) => (
@@ -115,30 +151,30 @@ const CustomeEmployeeDetails = ({
                     <>
                       <TableRow
                         hover
-                        role="checkbox"
+                        role='checkbox'
                         tabIndex={-1}
                         key={row.code}
                       >
                         {columns?.map((column) => {
                           const value = row[column.id];
-                          if (column?.id === "actions") {
+                          if (column?.id === 'actions') {
                             return (
                               <TableCell key={column.id} align={column.align}>
                                 <div
                                   style={{
-                                    display: "flex",
-                                    gap: "12px",
-                                    justifyContent: "center",
-                                    alignItems: "center",
+                                    display: 'flex',
+                                    gap: '12px',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
                                   }}
                                 >
                                   <ModeEditOutlineIcon
                                     sx={{
                                       color:
-                                        mode !== "dark" ? "black" : "#fcfcfc",
-                                      cursor: "pointer",
-                                      "&:hover": {
-                                        color: "green",
+                                        mode !== 'dark' ? 'black' : '#fcfcfc',
+                                      cursor: 'pointer',
+                                      '&:hover': {
+                                        color: 'green',
                                       },
                                     }}
                                     onClick={() => handleEdit(row)}
@@ -146,10 +182,10 @@ const CustomeEmployeeDetails = ({
                                   <DeleteIcon
                                     sx={{
                                       color:
-                                        mode !== "dark" ? "black" : "#fcfcfc",
-                                      cursor: "pointer",
-                                      "&:hover": {
-                                        color: "red",
+                                        mode !== 'dark' ? 'black' : '#fcfcfc',
+                                      cursor: 'pointer',
+                                      '&:hover': {
+                                        color: 'red',
                                       },
                                     }}
                                     onClick={() => deleteCallBack(row)}
@@ -168,46 +204,11 @@ const CustomeEmployeeDetails = ({
                       </TableRow>
                       {showDocumentImg && (
                         <TableRow>
-                          {(() => {
-                            const {
-                              transcriptPath,
-                              otherDocumentPath,
-                              characterCertificatePath,
-                            } = row;
-
-                            const documentName = transcriptPath
-                              ? "Transcript"
-                              : otherDocumentPath
-                              ? "Other Document"
-                              : characterCertificatePath
-                              ? "Character Certificate"
-                              : "";
-
-                            const path = transcriptPath
-                              ? transcriptPath
-                              : otherDocumentPath
-                              ? otherDocumentPath
-                              : characterCertificatePath
-                              ? characterCertificatePath
-                              : "";
-
-                            if (
-                              !transcriptPath &&
-                              !otherDocumentPath &&
-                              !characterCertificatePath
-                            ) {
-                              return "";
-                            } else {
-                              return (
-                                <TableCell>
-                                  <ShowImagePreview
-                                    name={documentName}
-                                    path={path}
-                                  />
-                                </TableCell>
-                              );
-                            }
-                          })()}
+                          {documentData.map((document, index) => (
+                            <TableCell key={index}>
+                              <ShowImagePreview documentData={document} />
+                            </TableCell>
+                          ))}
                         </TableRow>
                       )}
                     </>
@@ -216,7 +217,7 @@ const CustomeEmployeeDetails = ({
               ) : (
                 <TableRow>
                   <TableCell></TableCell>
-                  <TableCell align="center">No Record Found</TableCell>
+                  <TableCell align='center'>No Record Found</TableCell>
                 </TableRow>
               )}
             </TableBody>
@@ -225,8 +226,8 @@ const CustomeEmployeeDetails = ({
       </Paper>
       {openAddModal && (
         <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
+          aria-labelledby='transition-modal-title'
+          aria-describedby='transition-modal-description'
           open={openAddModal}
           onClose={handleCloseAddModal}
           closeAfterTransition
@@ -241,26 +242,26 @@ const CustomeEmployeeDetails = ({
             <Box sx={style}>
               <Grid
                 sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  marginBottom: "1rem",
-                  position: "relative",
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  marginBottom: '1rem',
+                  position: 'relative',
                 }}
               >
                 <Typography
-                  variant="h6"
-                  color={mode === "dark" ? "#fcfcfc" : ""}
+                  variant='h6'
+                  color={mode === 'dark' ? '#fcfcfc' : ''}
                 >
-                  {selectedRowId ? "Edit" : "Add"} {title}
+                  {selectedRowId ? 'Edit' : 'Add'} {title}
                 </Typography>
                 <div
                   style={{
-                    width: "100%",
-                    height: "1px",
-                    backgroundColor: "#e0e0e0",
-                    position: "absolute",
-                    bottom: "0",
+                    width: '100%',
+                    height: '1px',
+                    backgroundColor: '#e0e0e0',
+                    position: 'absolute',
+                    bottom: '0',
                   }}
                 />
                 <IconButton onClick={handleCloseAddModal}>
--- src/app/utils/CustomeDetails/ShowImagePreview.jsx
+++ src/app/utils/CustomeDetails/ShowImagePreview.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import { DOC_URL } from "../../../auth/axiosInterceptor";
-import { Box, Fade, IconButton, Modal, Typography } from "@mui/material";
-import { BsEye } from "react-icons/bs";
-import "./ShowImagePreview.css";
-import CloseIcon from "@mui/icons-material/Close";
+import React, { useState } from 'react';
+import { DOC_URL } from '../../../auth/axiosInterceptor';
+import { Box, Fade, IconButton, Modal, Typography } from '@mui/material';
+import { BsEye } from 'react-icons/bs';
+import './ShowImagePreview.css';
+import CloseIcon from '@mui/icons-material/Close';
 
-const ShowImagePreview = ({ path, name }) => {
+const ShowImagePreview = ({ documentData }) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isPreviewOpen, setPreviewOpen] = useState(false);
 
@@ -23,58 +23,58 @@ const ShowImagePreview = ({ path, name }) => {
     setPreviewOpen(false);
   };
   const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "95%",
-    height: "80%",
-    bgcolor: "background.paper",
-    border: "1px solid #808080",
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '95%',
+    height: '80%',
+    bgcolor: 'background.paper',
+    border: '1px solid #808080',
     borderRadius: 2,
     boxShadow: 24,
-    p: "12px 24px",
+    p: '12px 24px',
   };
   return (
     <div>
       <Box
         sx={{
-          display: "flex",
-          alignItems: "center",
-          gap: "1rem",
+          display: 'flex',
+          alignItems: 'center',
+          gap: '1rem',
         }}
       >
-        <Typography variant="h6">{name}</Typography>
+        <Typography variant='h6'>{name}</Typography>
         <div
-          className="image-preview-container"
+          className='image-preview-container'
           onMouseEnter={handleHover}
           onMouseLeave={handleMouseLeave}
         >
-          <img src={DOC_URL + path} alt={name} />
+          <img src={DOC_URL + documentData?.path} alt={documentData?.name} />
           <div
-            className={`overlay-container ${isHovered ? "visible" : ""}`}
+            className={`overlay-container ${isHovered ? 'visible' : ''}`}
             onClick={openPreview}
           >
-            <BsEye color="white" size={30} />
+            <BsEye color='white' size={30} />
           </div>
         </div>
       </Box>
       <Modal
         open={isPreviewOpen}
         onClose={closePreview}
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
+        aria-labelledby='transition-modal-title'
+        aria-describedby='transition-modal-description'
         closeAfterTransition
       >
         <Fade in={isPreviewOpen}>
           <Box sx={style}>
             <div
               style={{
-                display: "flex",
-                justifyContent: "end",
-                width: "100%",
-                height: "1px",
-                bottom: "0",
+                display: 'flex',
+                justifyContent: 'end',
+                width: '100%',
+                height: '1px',
+                bottom: '0',
               }}
             >
               <IconButton onClick={closePreview}>
@@ -82,11 +82,11 @@ const ShowImagePreview = ({ path, name }) => {
               </IconButton>
             </div>
             <div>
-              {" "}
+              {' '}
               <img
-                style={{ objectFit: "cover" }}
-                src={DOC_URL + path}
-                alt={name}
+                style={{ objectFit: 'cover' }}
+                src={DOC_URL + documentData?.path}
+                alt={documentData?.name}
               />
             </div>
           </Box>
