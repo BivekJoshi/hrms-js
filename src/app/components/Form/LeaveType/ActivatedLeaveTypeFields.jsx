aed9a0702ca5921a2e4f78d21562b817451c6794
ganesh bhatt <@ganesh.bhatta>
2024-01-22 14:21:39 +0545

    rows size changed

--- src/app/components/Form/LeaveType/ActivatedLeaveTypeFields.jsx
+++ src/app/components/Form/LeaveType/ActivatedLeaveTypeFields.jsx
@@ -1,23 +1,36 @@
 import { Button, Grid, Typography } from "@mui/material";
 import React from "react";
-import { useActivateLeaveTypeForm, useDeactivateLeaveTypeForm } from './useDeleteLeaveTypeForm';
+import {
+  useActivateLeaveTypeForm,
+  useDeactivateLeaveTypeForm,
+} from "./useDeleteLeaveTypeForm";
 
 export const ActivatedLeaveTypeFields = ({ onClose, isLoading, data }) => {
-    const {formik }=  useActivateLeaveTypeForm(data, onClose);
+  const { formik } = useActivateLeaveTypeForm(data, onClose);
   const handleFormSubmit = () => {
     formik.handleSubmit();
 
     if (formik.isValid) {
-    //   onClose();
+      //   onClose();
     }
   };
 
   return (
     !isLoading && (
       <Grid container spacing={2}>
-        <Typography style={{ padding: "1rem" }} variant="p">
-          Are you sure you want activate this leave type
-        </Typography>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          sx={{ padding: "0 2rem", textAlign: "center" }}
+        >
+          <Typography variant="p">
+            Are you sure you want activate this leave type?
+          </Typography>
+          <Typography variant="h6" textAlign="center">
+            Leave Type : {data?.leaveName}
+          </Typography>
+        </Grid>
         <Grid
           item
           xs={12}
@@ -31,14 +44,14 @@ export const ActivatedLeaveTypeFields = ({ onClose, isLoading, data }) => {
           <Button
             variant="contained"
             onClick={handleFormSubmit}
-            sx={{ mt: 3, ml: 1 }}
+            // sx={{ mt: 3, ml: 1 }}
           >
             Yes Proceed
           </Button>
           <Button
             variant="contained"
             onClick={onClose}
-            sx={{ mt: 3, ml: 1 }}
+            sx={{ ml: 1 }}
             color="error"
           >
             No
@@ -50,48 +63,59 @@ export const ActivatedLeaveTypeFields = ({ onClose, isLoading, data }) => {
 };
 
 export const DeactivatedLeaveTypeFields = ({ onClose, isLoading, data }) => {
-  const {formik }=  useDeactivateLeaveTypeForm(data, onClose);
-const handleFormSubmit = () => {
-  formik.handleSubmit();
+  const { formik } = useDeactivateLeaveTypeForm(data, onClose);
+  const handleFormSubmit = () => {
+    formik.handleSubmit();
 
-  if (formik.isValid) {
-  //   onClose();
-  }
-};
+    if (formik.isValid) {
+      //   onClose();
+    }
+  };
+  console.log(data);
 
-return (
-  !isLoading && (
-    <Grid container spacing={2}>
-      <Typography style={{ padding: "1rem" }} variant="p">
-        Are you sure you want deactivate this leave type
-      </Typography>
-      <Grid
-        item
-        xs={12}
-        sm={12}
-        md={12}
-        container
-        direction="row"
-        justifyContent="flex-end"
-        alignItems="flex-end"
-      >
-        <Button
-          variant="contained"
-          onClick={handleFormSubmit}
-          sx={{ mt: 3, ml: 1 }}
+  return (
+    !isLoading && (
+      <Grid container spacing={2}>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          sx={{ padding: "0 2rem", textAlign: "center" }}
         >
-          Yes Proceed
-        </Button>
-        <Button
-          variant="contained"
-          onClick={onClose}
-          sx={{ mt: 3, ml: 1 }}
-          color="error"
+          <Typography variant="p">
+            Are you sure you want deactivate this leave type?
+          </Typography>
+          <Typography variant="h6" textAlign="center">
+            Leave Type : {data?.leaveName}
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={12}
+          container
+          direction="row"
+          justifyContent="flex-end"
+          alignItems="flex-end"
         >
-          No
-        </Button>
+          <Button
+            variant="contained"
+            onClick={handleFormSubmit}
+            // sx={{ mt: 1, ml: 1 }}
+          >
+            Yes Proceed
+          </Button>
+          <Button
+            variant="contained"
+            onClick={onClose}
+            sx={{ ml: 1 }}
+            color="error"
+          >
+            No
+          </Button>
+        </Grid>
       </Grid>
-    </Grid>
-  )
-);
+    )
+  );
 };
