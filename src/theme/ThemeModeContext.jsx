9b34bc6d40fd4e1966b3c6dabdcf62805130bf99
sanjay.thakur <sanjay.thakur@dghub.io>
2024-01-24 16:23:14 +0545

    fixes

--- src/theme/ThemeModeContext.jsx
+++ src/theme/ThemeModeContext.jsx
@@ -4,12 +4,12 @@ import React, {
   useEffect,
   useMemo,
   useState,
-} from 'react';
-import { responsiveFontSizes, ThemeProvider, createTheme } from '@mui/material';
+} from "react";
+import { responsiveFontSizes, ThemeProvider, createTheme } from "@mui/material";
 
 export const ThemeModeContext = createContext({
   toggleMode: () => {},
-  mode: 'light',
+  mode: "light",
   isMobile: false,
   toggleMobile: () => {},
   palette: {}, // Empty palette initially
@@ -18,43 +18,48 @@ export const ThemeModeContext = createContext({
 // Define getPalette outside the ThemeContextProvider
 const getPalette = (darkMode) => {
   return {
-    mode: darkMode ? 'dark' : 'light',
+    mode: darkMode ? "dark" : "light",
     primary: {
-      main: darkMode ? '#7bb338' : '#388E3C',
-      light: darkMode ? '#f0ca92' : '#6DAB23',
-      dark: darkMode ? '#98c464' : '#7bb338',
-      holiday: darkMode ? '' : '#FF8A7B',
+      main: darkMode ? "#7bb338" : "#388E3C",
+      light: darkMode ? "#f0ca92" : "#6DAB23",
+      dark: darkMode ? "#98c464" : "#7bb338",
+      holiday: darkMode ? "" : "#FF8A7B",
     },
     secondary: {
-      main: darkMode ? '#10598E' : '#98D8F1',
-      light: darkMode ? '#75baf0' : '#418fdd',
-      dark: darkMode ? '#336cb8' : '#336cb8',
+      main: darkMode ? "#10598E" : "#98D8F1",
+      light: darkMode ? "#75baf0" : "#418fdd",
+      dark: darkMode ? "#336cb8" : "#336cb8",
     },
     background: {
-      default: darkMode ? '#303030' : '#f8f8f8',
-      paper: darkMode ? '#292929' : '#fff',
-      imageCaption: darkMode ? '#c5c7cb' : '#616161',
-      tabbg: darkMode ? '#3D4852' : '#E8F5E9',
-      activetabBg: darkMode ? '#388E3C' : '#C8E6C9',
-      event: darkMode ? '#b3f594' : '#D5FFCB',
-      holiday: darkMode ? '#eeccc6' : '#FFEDEA',
-      toDo: darkMode ? '' : '#F2F4F4',
+      default: darkMode ? "#303030" : "#f8f8f8",
+      paper: darkMode ? "#292929" : "#fff",
+      imageCaption: darkMode ? "#c5c7cb" : "#616161",
+      tabbg: darkMode ? "#3D4852" : "#E8F5E9",
+      activetabBg: darkMode ? "#388E3C" : "#C8E6C9",
+      event: darkMode ? "#b3f594" : "#D5FFCB",
+      holiday: darkMode ? "#eeccc6" : "#FFEDEA",
+      toDo: darkMode ? "" : "#F2F4F4",
     },
     text: {
-      primary: darkMode ? '#FFFFFF' : '#000',
-      secondary: darkMode ? '#fff' : '#616161',
-      white: '#000',
-      tableHead: darkMode ? '#fff' : '#25262E',
+      primary: darkMode ? "#FFFFFF" : "#000",
+      secondary: darkMode ? "#fff" : "#616161",
+      white: "#000",
+      tableHead: darkMode ? "#fff" : "#25262E",
     },
-    divider: darkMode ? 'rgba(255,255,255,0.12)' : 'rgba(0,0,0,0.12)',
+    divider: darkMode ? "rgba(255,255,255,0.12)" : "rgba(0,0,0,0.12)",
   };
 };
 
 export const ThemeContextProvider = ({ children }) => {
-  const storedMode = localStorage.getItem('mode');
-  const [mode, setMode] = useState(storedMode || 'light');
+  const storedMode =
+    localStorage.getItem("mode") === null
+      ? "light"
+      : localStorage.getItem("mode");
 
-  const [isDarkMode, setIsDarkMode] = useState(mode === 'dark');
+  const [mode, setMode] = useState(storedMode || "light");
+  console.log("🚀 ~ ThemeContextProvider ~ mode:", mode);
+
+  const [isDarkMode, setIsDarkMode] = useState(mode === "dark");
   const [width, setWidth] = useState(window.innerWidth);
   const [isMobile, setIsMobile] = useState(width <= 768);
 
@@ -64,17 +69,17 @@ export const ThemeContextProvider = ({ children }) => {
   }, [width]);
 
   useEffect(() => {
-    window.addEventListener('resize', handleWindowSizeChange);
+    window.addEventListener("resize", handleWindowSizeChange);
     return () => {
-      window.removeEventListener('resize', handleWindowSizeChange);
+      window.removeEventListener("resize", handleWindowSizeChange);
     };
   }, [handleWindowSizeChange]);
 
   const toggleMode = useCallback(() => {
-    const newMode = mode === 'light' ? 'dark' : 'light';
+    const newMode = mode === "light" ? "dark" : "light";
     setMode(newMode);
-    setIsDarkMode(newMode === 'dark');
-    localStorage.setItem('mode', newMode);
+    setIsDarkMode(newMode === "dark");
+    localStorage.setItem("mode", newMode);
   }, [mode]);
 
   const colorMode = useMemo(
@@ -94,8 +99,8 @@ export const ThemeContextProvider = ({ children }) => {
       MuiButton: {
         styleOverrides: {
           root: {
-            textTransform: 'none',
-            color: mode === 'dark' && '#f5f5f5',
+            textTransform: "none",
+            color: mode === "dark" && "#f5f5f5",
           },
         },
       },
